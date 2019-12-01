6238632374ad2b1a4f7e656172d2b8cf2bafa805
Stanislav Schmidt <>
2019-12-01 18:40:37 +0100

    day 1 solution

---
+++ 2019/README.md
@@ -0,0 +1,6 @@
+# Advent of Code 2019
+
+- Web: [AoC](https://adventofcode.com/2019)
+- Discussions: [Reddit](https://www.reddit.com/r/adventofcode/)
+- Language: JavaScript
+
---
+++ 2019/src/day_01.js
@@ -0,0 +1,30 @@
+// Read Data
+const fs = require('fs');
+let data = fs.readFileSync("../input/input_01.txt", "utf8");
+data = data.trim().split("\n").map(numStr => parseInt(numStr));
+
+
+function getFuel(x) {
+    return Math.floor(x / 3) - 2
+}
+
+
+let total;
+// Part 1
+total = 0;
+for (let i = 0; i < data.length; ++i) {
+    total += getFuel(data[i]);
+}
+console.log("Part 1:", total);
+
+
+// Part 2;
+total = 0;
+for (let i = 0; i < data.length; ++i) {
+    let fuel = getFuel(data[i]);
+    while (fuel > 0) {
+        total += fuel;
+        fuel = getFuel(fuel);
+    }
+}
+console.log("Part 2:", total);
