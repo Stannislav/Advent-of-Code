dda51a4648cf78c020f1ba3c4eb74a414c5784c8
Stanislav Schmidt <stanislav.schmidt@outlook.com>
2020-12-05 10:35:44 +0100

    Add docstrings to day 4

--- 2020/src/bin/04.rs
+++ 2020/src/bin/04.rs
@@ -4,8 +4,20 @@ use regex::Regex;
 use std::collections::HashMap;
 use std::fs;
 
+/// Parse a passport entry.
+///
+/// First we extract a vector with all key-value pairs of the form
+/// `key:value` by splitting the data on spaces and new lines. Then
+/// all such pairs are split on `:` and inserted into a hash map.
+///
+/// # Arguments
+/// * `passport_data` -- Part of the input data representing one passport.
+///
+/// # Returns
+/// * `HashMap<String, String>` -- The parsed passport data as a key-value map.
 fn parse_passport(passport_data: &str) -> HashMap<String, String> {
-    let entries: Vec<String> = passport_data.trim()
+    let entries: Vec<String> = passport_data
+        .trim()
         .to_string()
         .split(|x| x == '\n' || x == ' ')
         .map(|x| x.to_string())
@@ -20,17 +32,34 @@ fn parse_passport(passport_data: &str) -> HashMap<String, String> {
     passport
 }
 
-/// byr (Birth Year) - four digits; at least 1920 and at most 2002.
-/// iyr (Issue Year) - four digits; at least 2010 and at most 2020.
-/// eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
-/// hgt (Height) - a number followed by either cm or in:
-///     If cm, the number must be at least 150 and at most 193.
-///     If in, the number must be at least 59 and at most 76.
-/// hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
-/// ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
-/// pid (Passport ID) - a nine-digit number, including leading zeroes.
-/// cid (Country ID) - ignored, missing or not.
-fn check_passport(passport: &HashMap<String, String>, check_values: bool) -> i32 {
+/// Check passport for part 1 and 2.
+///
+/// For part one (`check_values = false`) we only need to check for
+/// the presence of all required keys in the hash map.
+///
+/// For part two (`check_values = true`) we need to perform a specific
+/// check for each of the values. The requirements are stated in the
+/// problem text as follows:
+///
+/// * byr (Birth Year) -- four digits; at least 1920 and at most 2002.
+/// * iyr (Issue Year) -- four digits; at least 2010 and at most 2020.
+/// * eyr (Expiration Year) -- four digits; at least 2020 and at most 2030.
+/// * hgt (Height) -- a number followed by either cm or in:
+///   * If `cm`, the number must be at least 150 and at most 193.
+///   * If `in`, the number must be at least 59 and at most 76.
+/// * hcl (Hair Color) -- a # followed by exactly six characters 0-9 or a-f.
+/// * ecl (Eye Color) -- exactly one of: `amb` `blu` `brn` `gry` `gr`n `hzl` `oth`.
+/// * pid (Passport ID) -- a nine-digit number, including leading zeroes.
+/// * cid (Country ID) -- ignored, missing or not.
+///
+/// # Arguments
+/// * `passport` -- Passport data produced by [parse_passport](parse_passport).
+/// * `check_values` -- If `false` then only the keys are checked (part 1),
+///   otherwise also the values (part 2).
+///
+/// # Returns
+/// * `u8` -- Return 1 if the check passes, otherwise 0;
+fn check_passport(passport: &HashMap<String, String>, check_values: bool) -> u8 {
     for mandatory_field in ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"].iter() {
         if !passport.contains_key(&mandatory_field.to_string()) {
             return 0;
@@ -41,6 +70,7 @@ fn check_passport(passport: &HashMap<String, String>, check_values: bool) -> i32
         return 1;
     }
 
+    // Part 2 only
     // byr
     let byr: i32 = passport.get("byr").unwrap().parse().unwrap();
     if byr < 1920 || byr > 2002 {
@@ -90,6 +120,7 @@ fn check_passport(passport: &HashMap<String, String>, check_values: bool) -> i32
     return 1;
 }
 
+#[doc(hidden)]
 fn main() {
     // Read data
     let passports: Vec<HashMap<String, String>> = fs::read_to_string("input/04.txt")
