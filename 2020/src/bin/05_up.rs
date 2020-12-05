172422da43b017f45d061e576c5891d7b71c1109
Stanislav Schmidt <stanislav.schmidt@outlook.com>
2020-12-05 12:30:45 +0100

    Add improved day 5 solution

---
+++ 2020/src/bin/05_up.rs
@@ -0,0 +1,23 @@
+use std::fs;
+
+fn main() {
+    // Read input
+    let mut ids: Vec<usize> = fs::read_to_string("input/05.txt")
+        .expect("Can't read input file")
+        .trim()
+        .replace(&['F', 'L'][..], "0")
+        .replace(&['B', 'R'][..], "1")
+        .lines()
+        .map(|x| usize::from_str_radix(x, 2).expect("Can't parse line"))
+        .collect();
+
+    // Solutions
+    println!("Part 1: {}", ids.iter().max().unwrap());
+    ids.sort();
+    for (x1, x2) in ids.iter().zip(ids.iter().skip(1)) {
+        if x2 - x1 == 2 {
+            println!("Part 2: {}", x1 + 1);
+            break;
+        }
+    }
+}
