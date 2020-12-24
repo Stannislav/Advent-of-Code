118c3774f192d3d42f3f8efa64bced07ad8060f3
Stanislav Schmidt <stanislav.schmidt@epfl.ch>
2020-12-25 00:51:20 +0100

    Solve day 13

--- 2020/src/bin/13.rs
+++ 2020/src/bin/13.rs
@@ -1,14 +1,21 @@
+//! Solutions for day 13.
 use std::collections::HashMap;
 use std::fs;
 
+#[doc(hidden)]
 fn main() {
+    // Parse input
     let lines: Vec<String> = fs::read_to_string("input/13.txt")
         .expect("Can't read input")
         .lines()
         .map(|line| line.to_string())
         .collect();
+    // First line: timestamp
     let timestamp: usize = lines[0].parse().unwrap();
-    let ids: HashMap<usize, usize> = lines[1]
+    // Second line: bus IDs. We don't need the xs, only the bus IDs,
+    // and the offsets that they're at. We collect everything in a
+    // mapping (offset => bus ID)
+    let buses: HashMap<usize, usize> = lines[1]
         .trim()
         .split(',')
         .enumerate()
@@ -16,37 +23,52 @@ fn main() {
         .map(|(i, s)| (i, s.parse().unwrap()))
         .collect();
 
-    // let timestamp: usize = 939;
-    // let ids: Vec<usize> = vec![7,13,59,31,19].into_iter().collect();
-
     // Part 1
-    let mut min_wait = *(ids.values().max().unwrap());
-    let mut part_1: usize = 0;
-    for &id in ids.values() {
+    // We only need bus IDs. These IDs are equal to the time
+    // interval between the corresponding bus departures. All buses start
+    // driving at t = 0. Which bus is departing the earliest after the
+    // given timestamp?
+    // For every ID calculate how much wait one has. It can't be bigger than
+    // the smallest ID that we have.
+    let mut min_id = *(buses.values().min().unwrap());
+    let mut min_wait = min_id;
+    for &id in buses.values() {
+        // (timestamp % id) = t is the last time this bus was seen before the
+        // timestamp. Thus it will come again t' = (id - t) minutes after
+        // timestamp.
         let wait = id - (timestamp % id);
-        // println!("id = {}, wait = {}, min_wait = {}, {} {}", id, wait, min_wait, timestamp % id, timestamp);
         if wait < min_wait {
             min_wait = wait;
-            part_1 = id * min_wait;
+            min_id = id;
         }
     }
-    println!("Part 1: {}", part_1); // = 3035
+    println!("Part 1: {}", min_wait * min_id);
 
-    // Part 2 -- brute-force -- takes too long
-    let (_i, id) = ids.iter().max_by(|a, b| a.1.cmp(&b.1)).unwrap();
-    let mut timestamp = *id;
-    while !check(&ids, timestamp) {
-        timestamp += id;
-        // println!("{}", timestamp);
+    // Part 2
+    // In principle we can start with t = 0, keep increasing it by dt = 1
+    // and check every time if the offsets work out. However, this brute-force solution
+    // takes too long.
+    // Instead, focus on just one bus, and increate t until we hit the right offset. We
+    // would like to keep this offset, so we need to increase t by ID instead of by 1.
+    // Consder the next bus and its ID. Generally for the given t the offset will be wrong.
+    // We increase t (by the new dt!) until the offset works out also for this bus.
+    // Remember that the offset for the first bus doesn't change and stays correct too. To
+    // also keep this offset fixed we may only increase t by a certain number of dts. We can
+    // compute it manually by simply checking which period the remainders ID % dt have, but
+    // because all IDs are prime we know that the period is ID itself. So if we now update
+    // dt => dt * ID both offsets will stay fixed. Now proceed in exactly the same way to
+    // fix the offsets for the remaining buses. After all offsets have been fixed the
+    // timesteps is exactly the solution of the problem.
+    let mut timestamp = 0;
+    let mut delta = 1;
+    for (offset, id) in buses {
+        while (timestamp + offset) % id != 0 {
+            timestamp += delta;
+        }
+        // Normally we would need to calculate the cycle length, i.e. how many
+        // additions of delta it takes to get back to the same remainder. But because
+        // all IDs are prime the cycle is always going to be equal to the ID.
+        delta *= id;
     }
     println!("Part 2: {}", timestamp);
-}
-
-fn check(ids: &HashMap<usize, usize>, timestamp: usize) -> bool {
-    for (&i, &id) in ids.iter() {
-        if i != id - (timestamp % id) {
-            return false;
-        }
-    }
-    true
 }
