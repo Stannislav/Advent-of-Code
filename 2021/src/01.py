96a10378ceddc378730d347d9bbbd4a852be5253
Stanislav Schmidt <stanislav.schmidt@epfl.ch>
2021-12-01 11:40:25 +0100

    Make parameters positional/keyword only

--- 2021/src/01.py
+++ 2021/src/01.py
@@ -1,18 +1,18 @@
 """Solutions for day 1."""
 
 
-def grad(values: list[int]) -> list[int]:
+def grad(values: list[int], /) -> list[int]:
     """Compute the gradient of a sequence of values."""
     return [v2 - v1 for v1, v2 in zip(values, values[1:])]
 
 
-def sliding_sum(values: list[int], window_size: int) -> list[int]:
+def sliding_sum(values: list[int], *, window_size: int) -> list[int]:
     """Compute the sliding window sum of a sequence of values."""
     n_windows = len(values) - window_size + 1
     return [sum(values[i : i + window_size]) for i in range(n_windows)]
 
 
-def count_positive(values: list[int]) -> int:
+def count_positive(values: list[int], /) -> int:
     """Count the number of positive values in a sequence."""
     return sum(x > 0 for x in values)
 
@@ -23,7 +23,7 @@ def main() -> None:
         depths = [int(line) for line in fh]
 
     print(f"Part 1: {count_positive(grad(depths))}")
-    print(f"Part 2: {count_positive(grad(sliding_sum(depths, 3)))}")
+    print(f"Part 2: {count_positive(grad(sliding_sum(depths, window_size=3)))}")
 
 
 if __name__ == "__main__":
