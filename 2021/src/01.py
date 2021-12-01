835fccc0be939073fd0aa625f6dc14642682efe1
Stanislav Schmidt <stanislav.schmidt@epfl.ch>
2021-12-01 09:20:27 +0100

    Add type hints to solution

--- 2021/src/01.py
+++ 2021/src/01.py
@@ -1,23 +1,23 @@
 """Solutions for day 1."""
 
 
-def grad(values):
+def grad(values: list[int]) -> list[int]:
     """Compute the gradient of a sequence of values."""
     return [v2 - v1 for v1, v2 in zip(values, values[1:])]
 
 
-def sliding_sum(values, window_size):
+def sliding_sum(values: list[int], window_size: int) -> list[int]:
     """Compute the sliding window sum of a sequence of values."""
     n_windows = len(values) - window_size + 1
     return [sum(values[i : i + window_size]) for i in range(n_windows)]
 
 
-def count_positive(values):
+def count_positive(values: list[int]) -> int:
     """Count the number of positive values in a sequence."""
     return sum(x > 0 for x in values)
 
 
-def main():
+def main() -> None:
     """Compute the solutions."""
     with open("input/01.txt") as fh:
         depths = [int(line) for line in fh]
