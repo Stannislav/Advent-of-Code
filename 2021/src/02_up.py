6bd059f7bda60400875d5db6e620fd5e16c0d294
Stanislav Schmidt <stanislav.schmidt@epfl.ch>
2021-12-02 14:50:18 +0100

    Add community-based day 2 solution

---
+++ 2021/src/02_up.py
@@ -0,0 +1,45 @@
+"""Community-inspired solution to day 2."""
+from typing import TypeAlias
+
+Command: TypeAlias = tuple[str, int]
+
+
+def solution(commands: list[Command]) -> tuple[int, int]:
+    """Perform piloting according to part 2 instructions.
+
+    Compute the solutions for both parts in one pass by realising
+    that `aim` from part 2 is equivalent to `depth` from part 1.
+
+    :param commands: The commands from the problem input.
+    :return: Part 1 solution.
+    :return: Part 2 solution.
+    """
+    pos, depth, aim = 0, 0, 0
+    for cmd, value in commands:
+        match cmd:
+            case "forward":
+                pos += value
+                depth += aim * value
+            case "down":
+                aim += value
+            case "up":
+                aim -= value
+
+    return pos * aim, pos * depth
+
+
+def main() -> None:
+    """Compute the solutions."""
+    commands = []
+    with open("input/02.txt") as fh:
+        for line in fh:
+            cmd, value = line.split()
+            commands.append((cmd, int(value)))
+
+    part1, part2 = solution(commands)
+    print("Part 1:", part1)
+    print("Part 2:", part2)
+
+
+if __name__ == "__main__":
+    main()
