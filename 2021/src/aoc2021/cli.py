61890355140e6c70da82cbc5ce10db74cd6414cb
Stanislav Schmidt <stanislav.schmidt@epfl.ch>
2021-12-05 16:13:31 +0100

    Improve the CLI

--- 2021/src/aoc2021/cli.py
+++ 2021/src/aoc2021/cli.py
@@ -1,28 +1,49 @@
 """The command line application for running the advent of code solutions."""
 import argparse
 import importlib
+import pathlib
 from typing import cast
 
 from aoc2021.lib import ModSolution
 
 
-def main() -> None:
+def main() -> int:
     """Run the main CLI entry point."""
-    parser = argparse.ArgumentParser()
-    parser.add_argument("day", type=int)
-    parser.add_argument("-e", "--extra", action="store_true")
+    parser = argparse.ArgumentParser(
+        description="Run the advent of code puzzle solutions."
+    )
+    parser.add_argument("day", type=int, help="the day number")
+    parser.add_argument(
+        "-e",
+        "--extra",
+        action="store_true",
+        help="run the alternative community-based solution",
+    )
     args = parser.parse_args()
 
-    if args.extra:
-        module = "solutions_extra"
-    else:
-        module = "solutions"
-    module = f"aoc2021.{module}.day{args.day:02d}"
-
-    mod = cast(ModSolution, importlib.import_module(module))
-    with open(f"input/{args.day:02d}.txt") as fh:
+    # Read the input data
+    data_file = pathlib.Path(f"input/{args.day:02d}.txt")
+    if not data_file.exists():
+        print(f"Input data file not found: {data_file}")
+        return 1
+    with data_file.open() as fh:
         raw_data = fh.read()
 
-    part1, part2 = mod.run(raw_data)
+    # Load the solution module
+    if args.extra:
+        submodule = "solutions_extra"
+    else:
+        submodule = "solutions"
+    module = f"aoc2021.{submodule}.day{args.day:02d}"
+    try:
+        mod_solution = cast(ModSolution, importlib.import_module(module))
+    except ModuleNotFoundError as exc:
+        print(exc)
+        return 1
+
+    # Get the solutions
+    part1, part2 = mod_solution.run(raw_data)
     print(f"Part 1: {part1}")
     print(f"Part 2: {part2}")
+
+    return 0
