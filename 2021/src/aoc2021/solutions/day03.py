bf3242f9c7837fe2538541fbb83c37f5361ca18c
Stanislav Schmidt <stanislav.schmidt@epfl.ch>
2021-12-05 15:57:47 +0100

    Make the bit width parametrizable in day 3

--- 2021/src/aoc2021/solutions/day03.py
+++ 2021/src/aoc2021/solutions/day03.py
@@ -1,7 +1,4 @@
 """Solutions for day 3."""
-N_BITS = 12
-BIT_MASK = (1 << N_BITS) - 1  # e.g. 0b111 for N_BITS = 3
-
 Data = list[int]
 
 
@@ -12,12 +9,21 @@ def most_common(numbers: Data, bit_pos: int) -> int:
     return int(count * 2 >= len(numbers))
 
 
-def inv(n: int) -> int:
-    """Compute the bitwise inverse."""
-    return ~n & BIT_MASK
+def inv(n: int, n_bits: int) -> int:
+    """Compute the bitwise inverse.
+
+    :param n: An integer.
+    :param n_bits: The bit-width of the integers used.
+    :returns: The binary inverse of the input.
+    """
+    # We should only invert the bits that are within the bit-width of the
+    # integers we use. We set this mask to set the other bits to zero.
+    bit_mask = (1 << n_bits) - 1  # e.g. 0b111 for n_bits = 3
+
+    return ~n & bit_mask
 
 
-def part1(data: Data) -> int:
+def part1(data: Data, n_bits: int) -> int:
     """Compute the part 1 solution.
 
     The gamma rate is an integer that consists of the most common bits
@@ -25,25 +31,28 @@ def part1(data: Data) -> int:
     in the input data and is therefore the bitwise inverse of the gamma rate.
 
     :param data: The puzzle input data.
+    :param n_bits: The bit-width of the integers used.
     :return: The part 1 solution.
     """
     gamma = 0
-    for bit_pos in range(N_BITS):
+    for bit_pos in range(n_bits):
         gamma |= most_common(data, bit_pos) << bit_pos
-    epsilon = inv(gamma)
+
+    epsilon = inv(gamma, n_bits)
 
     return gamma * epsilon
 
 
-def keep_most_common(values: Data, *, least_common: bool = False) -> int:
+def keep_most_common(values: Data, n_bits: int, *, least_common: bool = False) -> int:
     """Run the bit criteria loop for part 2.
 
     :param values: The puzzle input data.
+    :param n_bits: The bit-width of the integers used.
     :param least_common: If true the criteria is the least common bit,
     otherwise it's the most common bit.
     :return: The single remaining value after the criteria loop.
     """
-    pos = N_BITS
+    pos = n_bits
     while len(values) > 1:
         pos -= 1
         bit_want = most_common(values, pos)
@@ -55,16 +64,18 @@ def keep_most_common(values: Data, *, least_common: bool = False) -> int:
     return values.pop()
 
 
-def part2(data: list[int]) -> int:
+def part2(data: list[int], n_bits: int) -> int:
     """Compute the part 2 solution."""
-    oxygen = keep_most_common(data)
-    co2 = keep_most_common(data, least_common=True)
+    oxygen = keep_most_common(data, n_bits)
+    co2 = keep_most_common(data, n_bits, least_common=True)
 
     return oxygen * co2
 
 
 def run(raw_data: str) -> tuple[int, int]:
     """Solve the puzzles."""
-    data = [int(line, base=2) for line in raw_data.splitlines()]
+    lines = raw_data.splitlines()
+    n_bits = len(lines[0])
+    data = [int(line, base=2) for line in lines]
 
-    return part1(data), part2(data)
+    return part1(data, n_bits), part2(data, n_bits)
