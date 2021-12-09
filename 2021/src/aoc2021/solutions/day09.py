5965c173d49dcbd00526d9280c646b171b70aacc
Stanislav Schmidt <stanislav.schmidt@epfl.ch>
2021-12-09 09:59:09 +0100

    Add day 9 stubs

---
+++ 2021/src/aoc2021/solutions/day09.py
@@ -0,0 +1,9 @@
+"""Solutions for day 9."""
+
+
+def run(data_s: str) -> tuple[int, int]:
+    """Solve the puzzles."""
+    # Parse input
+    part1 = part2 = 0
+
+    return part1, part2
---
+++ 2021/tests/test_day09.py
@@ -0,0 +1,15 @@
+from aoc2021.solutions import day09
+
+data = """\
+2199943210
+3987894921
+9856789892
+8767896789
+9899965678
+"""
+
+
+def test_my_solution():
+    part1, part2 = day09.run(data)
+    assert part1 == 15
+    assert part2 == 0
