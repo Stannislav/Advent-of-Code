af9bc60e1ce4ea6cd3ba9d80ca22f72ec9df044e
Stanislav Schmidt <stanislav.schmidt@epfl.ch>
2021-12-09 21:51:25 +0100

    Add day 10 stubs

---
+++ 2021/src/aoc2021/solutions/day10.py
@@ -0,0 +1,11 @@
+"""Solutions for day 10."""
+
+
+def run(data_s: str) -> tuple[int, int]:
+    """Solve the puzzles."""
+    # Parse input
+
+    # Solve
+    part1 = part2 = 0
+
+    return part1, part2
