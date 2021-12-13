aedb28360173376a3e914fa236e2e0cc46f4c372
Stanislav Schmidt <stanislav.schmidt@epfl.ch>
2021-12-13 10:03:31 +0100

    Update types and docstrings in lib

--- 2021/src/aoc2021/lib.py
+++ 2021/src/aoc2021/lib.py
@@ -3,12 +3,14 @@ from typing import Protocol
 
 
 class ModSolution(Protocol):
-    """The protocol for a module with an AOC puzzle solution."""
+    """The protocol for a module with an Advent of Code puzzle solution."""
 
-    def run(self, data: str) -> tuple[int, int]:
+    def run(self, data: str) -> tuple[int | str, int | str]:
         """Solve the puzzle given the input data.
 
-        :param data: The puzzle input data.
-        :return: Solution to part 1.
-        :return: Solution to part 2.
+        Args:
+            data: The puzzle input data.
+
+        Returns:
+            A tuple with two elements - the solutions to part 1 and part2.
         """
