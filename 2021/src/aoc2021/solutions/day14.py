93d5402271cab3d9b12ee168fc9636cbf7742843
Stanislav Schmidt <stanislav.schmidt@epfl.ch>
2021-12-14 07:59:41 +0100

    Add day 14 stubs

---
+++ 2021/src/aoc2021/solutions/day14.py
@@ -0,0 +1,7 @@
+"""Solutions for day 14."""
+
+
+def run(data_s: str) -> tuple[int, str]:
+    """Solve the puzzles."""
+
+    return 0, 0
---
+++ 2021/tests/test_day14.py
@@ -0,0 +1,28 @@
+from aoc2021.solutions import day14
+
+data = """\
+NNCB
+
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C
+"""
+
+
+def test_my_solution():
+    part1, part2 = day14.run(data)
+    assert part1 == 1588
+    assert part2 == 0
