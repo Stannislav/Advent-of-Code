2e77525f1d6b921b6f53f44fb01bcd85e8cc68ce
Stanislav Schmidt <stanislav.schmidt@epfl.ch>
2021-12-15 07:59:14 +0100

    Add example solutions to tests

--- 2021/tests/test_day15.py
+++ 2021/tests/test_day15.py
@@ -1,10 +1,20 @@
 from aoc2021.solutions import day15
 
 data = """\
+1163751742
+1381373672
+2136511328
+3694931569
+7463417111
+1319128137
+1359912421
+3125421639
+1293138521
+2311944581
 """
 
 
 def test_my_solution():
     part1, part2 = day15.run(data)
-    assert part1 == 0
-    assert part2 == 0
+    assert part1 == 40
+    assert part2 == 315
