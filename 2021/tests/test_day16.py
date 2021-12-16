9d20ce75c52331ce95981f1e9d3f9b75c0f4e502
Stanislav Schmidt <stanislav.schmidt@epfl.ch>
2021-12-16 08:43:30 +0100

    Add day 16 part 1 example solutions

--- 2021/tests/test_day16.py
+++ 2021/tests/test_day16.py
@@ -1,10 +1,23 @@
+import pytest
+
 from aoc2021.solutions import day16
 
-data = """\
-"""
+data1 = "8A004A801A8002F478"
+data2 = "620080001611562C8802118E34"
+data3 = "C0015000016115A2E0802F182340"
+data4 = "A0016C880162017C3686B18A3D4780"
 
 
-def test_my_solution():
+@pytest.mark.parametrize(
+    ("data", "part1_expected", "part2_expected"),
+    (
+        (data1, 16, 0),
+        (data2, 12, 0),
+        (data3, 23, 0),
+        (data4, 31, 0),
+    )
+)
+def test_my_solution(data, part1_expected, part2_expected):
     part1, part2 = day16.run(data)
-    assert part1 == 0
-    assert part2 == 0
+    assert part1 == part1_expected
+    assert part2 == part2_expected
