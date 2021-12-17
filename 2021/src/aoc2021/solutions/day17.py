e1e45882417b2cff019a5bcb63be03cb0c29ca29
Stanislav Schmidt <stanislav.schmidt@epfl.ch>
2021-12-17 07:49:29 +0100

    Add day 17 stubs

---
+++ 2021/src/aoc2021/solutions/day17.py
@@ -0,0 +1,7 @@
+"""Solutions for day 17."""
+
+
+def run(data_s: str) -> tuple[int, int]:
+    """Solve the puzzles."""
+
+    return 0, 0
---
+++ 2021/tests/test_day17.py
@@ -0,0 +1,10 @@
+from aoc2021.solutions import day17
+
+data = """\
+"""
+
+
+def test_my_solution():
+    part1, part2 = day17.run(data)
+    assert part1 == 0
+    assert part2 == 0
