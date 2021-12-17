074f7773ecc73489d20e6969f84f89a5035d2630
Stanislav Schmidt <stanislav.schmidt@epfl.ch>
2021-12-17 10:58:56 +0100

    Add day 17 part 2 example solution to tests

--- 2021/tests/test_day17.py
+++ 2021/tests/test_day17.py
@@ -1,10 +1,9 @@
 from aoc2021.solutions import day17
 
-data = """\
-"""
+data = "target area: x=20..30, y=-10..-5"
 
 
 def test_my_solution():
     part1, part2 = day17.run(data)
-    assert part1 == 0
-    assert part2 == 0
+    assert part1 == 45
+    assert part2 == 112
