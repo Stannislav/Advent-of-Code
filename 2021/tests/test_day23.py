d0efd6d255b07c76ef5750229d879fb0f73d1837
Stanislav Schmidt <stanislav.schmidt@epfl.ch>
2021-12-23 21:10:29 +0100

    Add day 23 example solutions

--- 2021/tests/test_day23.py
+++ 2021/tests/test_day23.py
@@ -1,10 +1,15 @@
 from aoc2021.solutions import day23
 
 data = """\
+#############
+#...........#
+###B#C#B#D###
+  #A#D#C#A#
+  #########
 """
 
 
 def test_my_solution():
     part1, part2 = day23.run(data)
-    assert part1 == 0
-    assert part2 == 0
+    assert part1 == 12521
+    assert part2 == 44169
