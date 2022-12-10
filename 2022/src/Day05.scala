83045125c671e56de77fca28c9c4b9d3f8a5d83c
Stanislv Schmidt <Stannislav@users.noreply.github.com>
2022-12-10 23:05:07 +0100

    Fix code issues in day 5

--- 2022/src/Day05.scala
+++ 2022/src/Day05.scala
@@ -1,5 +1,4 @@
-import scala.collection.mutable.Stack
-
+import scala.collection.mutable
 
 object Day05 {
   def main(args: Array[String]): Unit = {
@@ -8,28 +7,28 @@ object Day05 {
       .mkString
       .split("\n\n")
       match {
-        case Array(a: String, b: String) => (parse_stacks(a), parse_instructions(b))
+        case Array(a: String, b: String) => (parseStacks(a), parseInstructions(b))
         case _ => throw Exception("Badly formatted input")
       }
     }
 
     // Part 1
-    val part1 = solve(clone_stacks(stacks), instructions, one_by_one = true)
+    val part1 = solve(cloneStacks(stacks), instructions, one_by_one = true)
     println(s"Part 1: $part1")
 
     // Part 2
-    val part2 = solve(clone_stacks(stacks), instructions, one_by_one = false)
+    val part2 = solve(cloneStacks(stacks), instructions, one_by_one = false)
     println(s"Part 2: $part2")
   }
 
 
-  def parse_stacks(input: String): List[Stack[Char]] = {
+  private def parseStacks(input: String): List[mutable.Stack[Char]] = {
     val lines = input.linesIterator.toList
     val height = lines.length - 1 // last row = labels
     val width = lines.map(_.strip.length).max
 
     Range(1, width, 4).map(col => {
-      var stack: Stack[Char] = Stack()
+      val stack: mutable.Stack[Char] = mutable.Stack()
       Range(0, height).reverse.foreach(row => {
         val c = lines(row)(col)
         if (c != ' ')
@@ -39,7 +38,7 @@ object Day05 {
     }).toList
   }
 
-  def parse_instructions(input: String): List[(Int, Int, Int)] = {
+  private def parseInstructions(input: String): List[(Int, Int, Int)] = {
     input
       .linesIterator
       .map {
@@ -49,7 +48,11 @@ object Day05 {
       .toList
   }
 
-  def solve(stacks: List[Stack[Char]], instructions: List[(Int, Int, Int)], one_by_one: Boolean): String = {
+  private def solve(
+    stacks: List[mutable.Stack[Char]],
+    instructions: List[(Int, Int, Int)],
+    one_by_one: Boolean,
+  ): String = {
     for ((n, from, to) <- instructions)
       val took = (1 to n).map(_ => stacks(from).pop)
       if (one_by_one)
@@ -60,5 +63,7 @@ object Day05 {
     stacks.map(stack => stack.top).mkString
   }
 
-  def clone_stacks(stacks: List[Stack[Char]]) = stacks.map(s => Stack().pushAll(s.reverse))
+  private def cloneStacks(stacks: List[mutable.Stack[Char]]) = {
+    stacks.map(s => mutable.Stack().pushAll(s.reverse))
+  }
 }
