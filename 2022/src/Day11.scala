055b3211f0d0f2512a8c292de06ce59c51269939
Stanislv Schmidt <Stannislav@users.noreply.github.com>
2022-12-11 22:02:27 +0100

    Solve day 11 part 2

--- 2022/src/Day11.scala
+++ 2022/src/Day11.scala
@@ -9,40 +9,42 @@ object Day11 {
       .toList
     }
 
-    for (_ <- Range(0, 20)) {
-      for ((monkey, i) <- monkeys.zipWithIndex) {
-        while(monkey.hasItems()) {
-          val (item, target) = monkey.step()
+    val mod = monkeys.map(_.divisor).product
+    println(s"Part 1: ${solve(monkeys, 20)}")
+    println(s"Part 2: ${solve(monkeys, 10000, mod)}")
+  }
+
+  private def solve(monkeys: Seq[Monkey], nRounds: Int, mod: Int = 1): Long = {
+    for (_ <- 1 to nRounds) {
+      for (monkey <- monkeys) {
+        while (monkey.hasItems) {
+          val (item, target) = monkey.step(mod)
           monkeys(target).receive(item)
         }
       }
     }
-    val part1 = monkeys
-      .map(_.nSteps())
-      .sorted
-      .reverse
-      .slice(0, 2)
-      .product
-    println(s"Part 1: $part1")
+    val solution = monkeys.map(_.nSteps).sorted.reverse.take(2).product
+    monkeys.foreach(_.reset())
+
+    solution
   }
 
   private def parseMonkey(input: String): Monkey = {
-    val pattern = """Monkey (\d+):
+    val pattern = """Monkey \d+:
                     |  Starting items: (.+)
                     |  Operation: new = (.+)
                     |  Test: divisible by (\d+)
                     |    If true: throw to monkey (\d+)
                     |    If false: throw to monkey (\d+)""".stripMargin
     input.strip match {
-      case pattern.r(i, items, op, divisor, throwTrue, throwFalse) =>
+      case pattern.r(items, op, divisor, throwTrue, throwFalse) =>
         Monkey(
-          i.toInt,
-          items.split(", ").map(_.toInt),
+          items.split(", ").toSeq.map(_.toLong),
           op match {
-            case "old + old" => (old: Int) => old + old
-            case "old * old" => (old: Int) => old * old
-            case s"old + $n" => (old: Int) => old + n.toInt
-            case s"old * $n" => (old: Int) => old * n.toInt
+            case "old + old" => (old: Long) => old + old
+            case "old * old" => (old: Long) => old * old
+            case s"old + $n" => (old: Long) => old + n.toInt
+            case s"old * $n" => (old: Long) => old * n.toInt
             case _ => throw Exception("Unknown op")
           },
           divisor.toInt,
@@ -54,29 +56,36 @@ object Day11 {
   }
 }
 
-type Op = Int => Int
+type Op = Long => Long
 
 class Monkey(
-  i: Int,
-  items: Iterable[Int],
+  initialItems: Seq[Long],
   op: Op,
-  divisor: Int,
+  val divisor: Int,
   throwTrue: Int,
   throwFalse: Int,
 ) {
-  private var nInspect = 0
-  private val itemStack = items.to(mutable.Stack)
+  var nSteps = 0L
+  private val items = initialItems.to(mutable.Stack)
 
-  override def toString(): String = s"Monkey $i: ${itemStack.mkString(", ")}"
-  def nSteps(): Int = nInspect
-  def hasItems(): Boolean = itemStack.nonEmpty
-  def receive(item: Int): Unit = itemStack.push(item)
-  def step(): (Int, Int) = {
-    val item = op(itemStack.pop) / 3
-    nInspect += 1
+  def hasItems: Boolean = items.nonEmpty
+  def receive(item: Long): Unit = items.push(item)
+  def step(mod: Int = 1): (Long, Int) = {
+    nSteps += 1
+    val item =
+      if (mod == 1)  // part 1
+        op(items.pop) / 3
+      else // part 2
+        op(items.pop) % mod
+
     if (item % divisor == 0)
       (item, throwTrue)
     else
       (item, throwFalse)
+  }
+  def reset(): Unit = {
+    nSteps = 0
+    items.clear()
+    items.pushAll(initialItems.reverse)
   }
 }
