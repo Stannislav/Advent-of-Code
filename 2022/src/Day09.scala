53a88d541dc04b097f2548ba93d048fbf232b496
Stanislv Schmidt <Stannislav@users.noreply.github.com>
2023-01-01 18:29:34 +0100

    Add TupleAdd.- in day 9

--- 2022/src/Day09.scala
+++ 2022/src/Day09.scala
@@ -5,6 +5,9 @@ import scala.collection.mutable.ListBuffer
 implicit class TupleAdd(self: (Int, Int)) {
   @targetName("add")
   def +(other: (Int, Int)): (Int, Int) = (self._1 + other._1, self._2 + other._2)
+
+  @targetName("sub")
+  def -(other: (Int, Int)): (Int, Int) = (self._1 - other._1, self._2 - other._2)
 }
 
 
