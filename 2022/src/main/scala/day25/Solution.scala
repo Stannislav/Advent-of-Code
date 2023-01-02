b5f2e49889102f6678014f74599b93450de967ed
Stanislv Schmidt <Stannislav@users.noreply.github.com>
2023-01-02 14:44:07 +0100

    Refactor day 25 solution

--- 2022/src/main/scala/day25/Solution.scala
+++ 2022/src/main/scala/day25/Solution.scala
@@ -2,10 +2,8 @@ package day25
 
 object Solution {
   def main(args: Array[String]): Unit = {
-    val snafus = util.Using.resource(io.Source.fromResource("input/25.txt")) {
-      _.getLines.map(SNAFU.parse).toList
-    }
-
+    val source = io.Source.fromResource("input/25.txt")
+    val snafus = try source.getLines.map(SNAFU.parse).toList finally source.close
     println(s"Part 1: ${snafus.reduce(_ + _)}")
   }
 }
