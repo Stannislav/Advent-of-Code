3c11124ab132e0be5ba5dbbb44dd27f8e2fe194f
Stanislav Schmidt <Stannislav@users.noreply.github.com>
2023-12-02 11:31:59 +0000

    Refactor day 2 solution

--- aoc2023/cmd/day02/main.go
+++ aoc2023/cmd/day02/main.go
@@ -9,45 +9,44 @@ import (
 )
 
 func main() {
-	data, _ := os.ReadFile("input/02.txt")
-	lines := strings.Split(strings.Trim(string(data), "\n"), "\n")
+	input, _ := os.ReadFile("input/02.txt")
+	lines := strings.Split(strings.Trim(string(input), "\n"), "\n")
 
-	reLine := regexp.MustCompile(`Game \d+: (.*)`)
-	reRed := regexp.MustCompile(`(\d+) red`)
-	reGreen := regexp.MustCompile(`(\d+) green`)
-	reBlue := regexp.MustCompile(`(\d+) blue`)
-
-	maxCount := []int{12, 13, 14}
+	reGame := regexp.MustCompile(`Game \d+: (.*)`)
+	maxCubes := []int{12, 13, 14}
 	idSum := 0
 	totalPower := 0
 	for id, line := range lines {
-		match := reLine.FindStringSubmatch(line)
 		isPossible := 1
-		minCount := []int{0, 0, 0}
-		for _, set := range strings.Split(match[1], "; ") {
-			drawnStr := [][]string{
-				reRed.FindStringSubmatch(set),
-				reGreen.FindStringSubmatch(set),
-				reBlue.FindStringSubmatch(set),
-			}
-			drawn := make([]int, 3)
-			for i, result := range drawnStr {
-				if len(result) > 1 {
-					drawn[i], _ = strconv.Atoi(result[1])
-				} else {
-					drawn[i] = 0
-				}
-			}
-			for i, count := range drawn {
-				minCount[i] = max(minCount[i], count)
-				if count > maxCount[i] {
+		minCubes := []int{0, 0, 0}
+		game := reGame.FindStringSubmatch(line)[1]
+		for _, hand := range strings.Split(game, "; ") {
+			cubesShown := parseHand(hand)
+			for i, nCubes := range cubesShown {
+				// for part 1
+				if nCubes > maxCubes[i] {
 					isPossible = 0
 				}
+				// for part 2
+				minCubes[i] = max(minCubes[i], nCubes)
 			}
 		}
-		totalPower += minCount[0] * minCount[1] * minCount[2]
+		totalPower += minCubes[0] * minCubes[1] * minCubes[2]
 		idSum += (id + 1) * isPossible
 	}
 	fmt.Println("Part 1:", idSum)
 	fmt.Println("Part 2:", totalPower)
+}
+
+// parseHand parse a hand of the form `"3 red, 5 green, 1 blue"` and
+// extract the cube count as an array `(3, 5, 1)`.
+func parseHand(hand string) (rbg []int) {
+	rbg = []int{0, 0, 0}
+	for i, color := range []string{"red", "green", "blue"} {
+		match := regexp.MustCompile(`(\d+) ` + color).FindStringSubmatch(hand)
+		if match != nil {
+			rbg[i], _ = strconv.Atoi(match[1])
+		}
+	}
+	return
 }
