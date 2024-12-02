2c45fea4d961d80aa9195073336abaf3da1178dd
Stanislav Schmidt <Stannislav@users.noreply.github.com>
2024-12-02 10:23:52 +0100

    Solve day 2

--- aoc2024/src/main/kotlin/day02/Day02.kt
+++ aoc2024/src/main/kotlin/day02/Day02.kt
@@ -2,11 +2,46 @@ package day02
 
 import java.io.File
 import java.io.InputStream
+import kotlin.math.absoluteValue
+import kotlin.math.sign
 
 fun main() {
-    parseInput(File("input/02.txt").inputStream())
+    val input = parseInput(File("input/02.txt").inputStream())
+    println("Part 1: ${part1(input)}")
+    println("Part 2: ${part2(input)}")
 }
 
-fun parseInput(stream: InputStream) {
+fun parseInput(stream: InputStream): List<List<Int>>  {
+    val input = mutableListOf<List<Int>>()
+    stream
+        .bufferedReader()
+        .forEachLine { line ->
+            input.add(line.split(" ").map { it.toInt() }.toList())
+        }
+    return input
+}
 
+fun part1(reports: List<List<Int>>): Int {
+    return reports.filter { isSafe(it) }.size
+}
+
+fun part2(reports: List<List<Int>>): Int {
+    return reports.filter { isSafe(it) || canTolerate(it) }.size
+}
+
+fun isSafe(report: List<Int>): Boolean {
+    val grad = report.zipWithNext().map {(a, b) -> b - a}
+    val isInRange = grad.all { it.absoluteValue in 1..3 }
+    val isMonotonic = grad.zipWithNext().all { (a, b) -> a.sign == b.sign }
+
+    return isInRange && isMonotonic
+}
+
+fun canTolerate(report: List<Int>): Boolean {
+    for (idxToDrop in report.indices) {
+        if (isSafe(report.filterIndexed { idx, _ -> idx != idxToDrop })) {
+            return true
+        }
+    }
+    return false
 }
--- aoc2024/src/test/kotlin/Day02Test.kt
+++ aoc2024/src/test/kotlin/Day02Test.kt
@@ -1,9 +1,33 @@
 import day02.parseInput
+import day02.part1
+import day02.part2
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class Day02Test {
     @Test
     fun testParseInput() {
-        parseInput(streamInput("02.txt"))
+        val input = parseInput(streamInput("02.txt"))
+        val expected = listOf(
+            listOf(7, 6, 4, 2, 1),
+            listOf(1, 2, 7, 8, 9),
+            listOf(9, 7, 6, 2, 1),
+            listOf(1, 3, 2, 4, 5),
+            listOf(8, 6, 4, 4, 1),
+            listOf(1, 3, 6, 7, 9),
+        )
+        assertEquals(expected, input)
+    }
+
+    @Test
+    fun testPart1() {
+        val input = parseInput(streamInput("02.txt"))
+        assertEquals(2, part1(input))
+    }
+
+    @Test
+    fun testPart2() {
+        val input = parseInput(streamInput("02.txt"))
+        assertEquals(4, part2(input))
     }
 }
