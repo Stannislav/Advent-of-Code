4ffe059fb66bdf7fdce10761da93eca3b9518b08
Stanislav Schmidt <Stannislav@users.noreply.github.com>
2024-12-03 10:09:58 +0100

    Simplify day 3

--- aoc2024/src/main/kotlin/day03/Day03.kt
+++ aoc2024/src/main/kotlin/day03/Day03.kt
@@ -12,33 +12,28 @@ fun part1(input: String): Int = findAll(input).sumOf { it.first * it.second }
 fun part2(input: String): Int = findAllConditional(input).sumOf { it.first * it.second }
 
 fun findAll(input: String): List<Pair<Int,Int>> {
-    val pattern = """mul\((\d+),(\d+)\)""".toRegex()
-    val result = mutableListOf<Pair<Int,Int>>()
-    val matches = pattern.findAll(input)
-    matches.forEach {
-        result.add(Pair(it.groupValues[1].toInt(), it.groupValues[2].toInt()))
-    }
-    return result
+    return """mul\((\d+),(\d+)\)"""
+        .toRegex()
+        .findAll(input)
+        .map {
+            Pair(it.groupValues[1].toInt(), it.groupValues[2].toInt())
+        }.toList()
 }
 
 fun findAllConditional(input: String): List<Pair<Int,Int>> {
-    val pattern = """(do\(\)|don't\(\))""".toRegex()
-    val matches = pattern.findAll(input)
     var updatedInput = ""
     var idx = 0
     var enabled = true
-    matches.forEach {
-        if (enabled) {
+    """(do\(\)|don't\(\))""".toRegex().findAll(input).forEach {
+        if (enabled)
             updatedInput += input.substring(idx..<it.range.first)
-        }
+        idx = it.range.last + 1
         when (it.value) {
             "do()" -> enabled = true
             "don't()" -> enabled = false
         }
-        idx = it.range.last + 1
     }
-    if (enabled) {
+    if (enabled)
         updatedInput += input.substring(idx..<input.length)
-    }
     return findAll(updatedInput)
 }
