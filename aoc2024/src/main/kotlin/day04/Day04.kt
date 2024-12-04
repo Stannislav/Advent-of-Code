80907cf0bdbdb0171aa0ea380249ec9c2c24c4fa
Stanislav Schmidt <Stannislav@users.noreply.github.com>
2024-12-04 10:24:12 +0100

    Rename input to grid

--- aoc2024/src/main/kotlin/day04/Day04.kt
+++ aoc2024/src/main/kotlin/day04/Day04.kt
@@ -17,45 +17,45 @@ fun parseInput(stream: InputStream): Array<CharArray> {
     return result
 }
 
-fun part1(input: Array<CharArray>): Int {
+fun part1(grid: Array<CharArray>): Int {
     var count = 0
 
     // horizontal
-    for (i in input.indices) {
-        for (j in 0 until input[i].size - 3) {
-            count += isXmas(input[i][j], input[i][j + 1], input[i][j + 2], input[i][j + 3])
+    for (i in grid.indices) {
+        for (j in 0 until grid[i].size - 3) {
+            count += isXmas(grid[i][j], grid[i][j + 1], grid[i][j + 2], grid[i][j + 3])
         }
     }
 
     // vertical
-    for (i in 0 until input.size - 3) {
-        for (j in 0 until input[0].size) {
-            count += isXmas(input[i][j], input[i + 1][j], input[i + 2][j], input[i + 3][j])
+    for (i in 0 until grid.size - 3) {
+        for (j in 0 until grid[0].size) {
+            count += isXmas(grid[i][j], grid[i + 1][j], grid[i + 2][j], grid[i + 3][j])
         }
     }
 
     // down-right
-    for (i in 0 until input.size - 3) {
-        for (j in 0 until input[0].size - 3) {
-            count += isXmas(input[i][j], input[i + 1][j + 1], input[i + 2][j + 2], input[i + 3][j + 3])
+    for (i in 0 until grid.size - 3) {
+        for (j in 0 until grid[0].size - 3) {
+            count += isXmas(grid[i][j], grid[i + 1][j + 1], grid[i + 2][j + 2], grid[i + 3][j + 3])
         }
     }
 
     // up-right
-    for (i in input.size-1 downTo 3) {
-        for (j in 0 until input[0].size - 3) {
-            count += isXmas(input[i][j], input[i - 1][j + 1], input[i - 2][j + 2], input[i - 3][j + 3])
+    for (i in grid.size-1 downTo 3) {
+        for (j in 0 until grid[0].size - 3) {
+            count += isXmas(grid[i][j], grid[i - 1][j + 1], grid[i - 2][j + 2], grid[i - 3][j + 3])
         }
     }
 
     return count
 }
 
-fun part2(input: Array<CharArray>): Int {
+fun part2(grid: Array<CharArray>): Int {
     var count = 0
-    for (i in 0 until input.size - 2) {
-        for (j in 0 until input[0].size - 2) {
-            count += isXmas(input, i, j)
+    for (i in 0 until grid.size - 2) {
+        for (j in 0 until grid[0].size - 2) {
+            count += isXmas(grid, i, j)
         }
     }
     return count
@@ -65,10 +65,10 @@ fun isXmas(c1: Char, c2: Char, c3: Char, c4: Char): Int {
     return if ((c1 == 'X' && c2 == 'M' && c3 == 'A' && c4 == 'S') || (c1 == 'S' && c2 == 'A' && c3 == 'M' && c4 == 'X')) 1 else 0
 }
 
-fun isXmas(input: Array<CharArray>, i: Int, j: Int): Int {
-    if (input[i + 1][j + 1] != 'A')
+fun isXmas(grid: Array<CharArray>, i: Int, j: Int): Int {
+    if (grid[i + 1][j + 1] != 'A')
         return 0
-    val edges = String(charArrayOf(input[i][j], input[i][j + 2], input[i + 2][j + 2], input[i + 2][j]))
+    val edges = String(charArrayOf(grid[i][j], grid[i][j + 2], grid[i + 2][j + 2], grid[i + 2][j]))
     if (edges == "MMSS" || edges == "SMMS" || edges == "SSMM" || edges == "MSSM")
         return 1
     return 0
