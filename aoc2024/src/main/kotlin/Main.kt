6f1fc9a9c11c67b4d44ff057a423aeae4394dc13
Stanislav Schmidt <Stannislav@users.noreply.github.com>
2024-12-09 23:26:12 +0100

    Add day 9 to main

--- aoc2024/src/main/kotlin/Main.kt
+++ aoc2024/src/main/kotlin/Main.kt
@@ -14,5 +14,7 @@ fun main() {
     println("--- Day 7: Bridge Repair ---")
     day07.main()
     println("--- Day 8: Resonant Collinearity ---")
-    day07.main()
+    day08.main()
+    println("--- Day 9: Disk Fragmenter ---")
+    day09.main()
 }
