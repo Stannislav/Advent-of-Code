6b99e01e4da568117fd5402ed5294f15170e2d09
Stanislav Schmidt <Stannislav@users.noreply.github.com>
2024-12-13 23:21:33 +0100

    Solve day 13

--- aoc2024/src/main/kotlin/day13/Day13.kt
+++ aoc2024/src/main/kotlin/day13/Day13.kt
@@ -1,5 +1,46 @@
 package day13
 
-fun main() {
+import java.io.File
+import java.io.InputStream
 
+
+fun main() {
+    val machines = parseInput(File("input/13.txt").inputStream())
+    println("Part 1: ${part1(machines)}")
+    println("Part 2: ${part2(machines)}")
+}
+
+data class Machine(val ax: Long, val ay: Long, val bx: Long, val by: Long, val x: Long, val y: Long) {
+    companion object {
+        fun fromString(str: String): Machine {
+            val match = """(\d+)""".toRegex().findAll(str)
+            val ints = match.map { it.value.toLong() }.toList()
+            check(ints.size == 6)
+            return Machine(ints[0], ints[1], ints[2], ints[3], ints[4], ints[5])
+        }
+    }
+
+    fun getCost(): Long? {
+        val det = ax * by - ay * bx
+        if (det == 0L) error("Buttons are collinear")
+
+        val inv = Pair(by * x - bx * y, -ay * x + ax * y)
+        return if (inv.first % det == 0L && inv.second % det == 0L)
+            (3 * inv.first + inv.second) / det
+        else
+            null
+    }
+}
+
+fun parseInput(stream: InputStream): List<Machine> {
+    return stream.bufferedReader().readText().split("\n\n").map { Machine.fromString(it) }
+}
+
+fun part1(machines: List<Machine>): Long {
+    return machines.mapNotNull { it.getCost() }.sum()
+}
+
+fun part2(machines: List<Machine>): Long {
+    val shift = 10000000000000L
+    return machines.map { it.copy(x = it.x + shift, y = it.y + shift) }.mapNotNull { it.getCost() }.sum()
 }
--- aoc2024/src/test/kotlin/Day13Test.kt
+++ aoc2024/src/test/kotlin/Day13Test.kt
@@ -1,2 +1,36 @@
+import day13.Machine
+import day13.parseInput
+import day13.part1
+import day13.part2
+import kotlin.test.*
+
 class Day13Test {
+    @Test
+    fun `Input parsing works`() {
+        val parsed = parseInput(streamInput("13.txt"))
+        val expected = listOf(
+            Machine(94, 34, 22, 67, 8400, 5400),
+            Machine(26, 66, 67, 21, 12748, 12176),
+            Machine(17, 86, 84, 37, 7870, 6450),
+            Machine(69, 23, 27, 71, 18641, 10279),
+        )
+        assertEquals(expected, parsed)
+    }
+
+    @Test
+    fun `Part 1 example works`() {
+        val machines = parseInput(streamInput("13.txt"))
+        assertEquals(480, part1(machines))
+    }
+
+    @Test
+    fun `Part 2 example works`() {
+        val shift = 10000000000000L
+        val machines = parseInput(streamInput("13.txt")).map { it.copy(x = it.x + shift, y = it.y + shift) }
+
+        assertNull(machines[0].getCost())
+        assertTrue(machines[1].getCost()!! > 100)
+        assertNull(machines[2].getCost())
+        assertTrue(machines[3].getCost()!! > 100)
+    }
 }
