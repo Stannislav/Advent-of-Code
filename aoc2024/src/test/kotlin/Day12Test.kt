7b0afed8874b78a81c296361c33648abb33629a2
Stanislav Schmidt <Stannislav@users.noreply.github.com>
2024-12-13 21:57:58 +0100

    Refactor tests

--- aoc2024/src/test/kotlin/Day12Test.kt
+++ aoc2024/src/test/kotlin/Day12Test.kt
@@ -1,51 +1,53 @@
 import common.Vec
 import day12.parseInput
 import day12.part1
+import day12.part2
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class Day12Test {
-    private val input1 = """
-        AAAA
-        BBCD
-        BBCC
-        EEEC
-    """.trimIndent()
-    private val input2 = """
-        OOOOO
-        OXOXO
-        OOOOO
-        OXOXO
-        OOOOO
-    """.trimIndent()
-    private val input3 = """
-        RRRRIICCFF
-        RRRRIICCCF
-        VVRRRCCFFF
-        VVRCCCJFFF
-        VVVVCJJCFE
-        VVIVCCJJEE
-        VVIIICJJEE
-        MIIIIIJJEE
-        MIIISIJEEE
-        MMMISSJEEE
-    """.trimIndent()
+    companion object {
+        @JvmStatic
+        fun part1() = listOf(
+            Arguments.of("1", 140),
+            Arguments.of("2", 772),
+            Arguments.of("3", 1930),
+        )
+
+        @JvmStatic
+        fun part2() = listOf(
+            Arguments.of("1", 80),
+            Arguments.of("2", 436),
+            Arguments.of("E", 236),
+            Arguments.of("AB", 368),
+            Arguments.of("3", 1206),
+        )
+    }
 
     @Test
-    fun `Should parse input correctly`() {
+    fun `Input parsing works`() {
         val expected = mapOf(
             Vec(0, 0) to 'A', Vec(0, 1) to 'A',Vec(0, 2) to 'A',Vec(0, 3) to 'A',
             Vec(1, 0) to 'B', Vec(1, 1) to 'B',Vec(1, 2) to 'C',Vec(1, 3) to 'D',
             Vec(2, 0) to 'B', Vec(2, 1) to 'B',Vec(2, 2) to 'C',Vec(2, 3) to 'C',
             Vec(3, 0) to 'E', Vec(3, 1) to 'E',Vec(3, 2) to 'E',Vec(3, 3) to 'C',
         )
-        assertEquals(expected, parseInput(input1.byteInputStream()))
+        assertEquals(expected, parseInput(streamInput("12-1.txt")))
     }
 
-    @Test
-    fun testPart1() {
-        assertEquals(140, part1(parseInput(input1.byteInputStream())))
-        assertEquals(772, part1(parseInput(input2.byteInputStream())))
-        assertEquals(1930, part1(parseInput(input3.byteInputStream())))
+    @ParameterizedTest(name = "Input {0} should give {2}")
+    @MethodSource("part1")
+    fun `Part 1 examples work`(name: String, expected: Int) {
+        assertEquals(expected, part1(parseInput(streamInput("12-$name.txt"))))
+    }
+
+
+    @ParameterizedTest(name = "Input {0} should give {2}")
+    @MethodSource("part2")
+    fun `Part 2 examples work`(name: String, expected: Int) {
+        assertEquals(expected, part2(parseInput(streamInput("12-$name.txt"))))
     }
 }
