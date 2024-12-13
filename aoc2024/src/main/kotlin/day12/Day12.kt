41125808ccd72c546881518b924fe3d932650eac
Stanislav Schmidt <Stannislav@users.noreply.github.com>
2024-12-13 22:21:05 +0100

    Solve day 12 part 2

--- aoc2024/src/main/kotlin/day12/Day12.kt
+++ aoc2024/src/main/kotlin/day12/Day12.kt
@@ -10,6 +10,7 @@ import kotlin.streams.asSequence
 fun main() {
     val input = parseInput(File("input/12.txt").inputStream())
     println("Part 1: ${part1(input)}")
+    println("Part 2: ${part2(input)}")
 }
 
 fun parseInput(stream: InputStream): Map<Vec, Char> {
@@ -57,4 +58,60 @@ fun perimeterScore(pos: Vec, input: Map<Vec, Char>): Int {
     val map = input.withDefault { '?' }
     return sequenceOf(UP, DOWN, LEFT, RIGHT)
         .count { map.getValue(pos) != map.getValue(pos + it) }
+}
+
+fun part2(input: Map<Vec, Char>): Int {
+    return getRegions(input).sumOf { it.size * getNumberOfSides(it) }
+}
+
+/**
+ * Compute number of straight sides of an area.
+ *
+ * The number of sides of an area is equal to the number of corners. Example:
+ *
+ * ```text
+ * +-+
+ * |C|
+ * | +-+
+ * |C C|
+ * +-+ |
+ *   |C|
+ *   +-+
+ * ```
+ *
+ * The area above has 8 straight sides, and also 8 corners. This is always true
+ * because straight sides and corners alternate.
+ *
+ * There are two types of corners - convex and concave corners.
+ * Convex corners have two tiles from a different region on either side, concave corners have
+ * tiles from the same region on either side, and a tile from a different region diagonally.
+ *
+ * Convex corner at top right of C:
+ *
+ * ```text
+ *
+ * d
+ * -+
+ * C|d
+ * ```
+ *
+ * Concave corner at top right of C:
+ *
+ * ```text
+ * c|D
+ *  +-
+ * C c
+ * ```
+ */
+fun getNumberOfSides(region: Set<Vec>): Int {
+    fun countCorners(pos: Vec): Int {
+        val same = { v: Vec -> region.contains(v) }
+        return sequenceOf(Pair(UP, LEFT), Pair(UP, RIGHT), Pair(DOWN, LEFT), Pair(DOWN, RIGHT))
+            .count {
+                val convex = !same(pos + it.first) && !same(pos + it.second)
+                val concave = same(pos + it.first) && same(pos + it.second) && !same(pos + it.first + it.second)
+                convex || concave
+            }
+    }
+    return region.sumOf { countCorners(it) }
 }
