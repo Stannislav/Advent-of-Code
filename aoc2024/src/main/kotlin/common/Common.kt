f2745692d0072fad0e03c78bb1f4575bc48850e9
Stanislav Schmidt <Stannislav@users.noreply.github.com>
2024-12-14 12:13:45 +0100

    Add Vec.times and Vec.mod to common, remove unused constants

--- aoc2024/src/main/kotlin/common/Common.kt
+++ aoc2024/src/main/kotlin/common/Common.kt
@@ -5,12 +5,10 @@ val Vec.i: Int get() = this.first
 val Vec.j: Int get() = this.second
 operator fun Vec.plus(other: Vec) = Vec(this.i + other.i, this.j + other.j)
 operator fun Vec.minus(other: Vec) = Vec(this.i - other.i, this.j - other.j)
+operator fun Vec.times(n: Int) = Vec(this.i * n, this.j * n)
+fun Vec.mod(other: Vec) = Vec((this.i.mod(other.i)), this.j.mod(other.j))
 
 val UP = Vec(-1, 0)
 val DOWN = Vec(1, 0)
 val LEFT = Vec(0, -1)
 val RIGHT = Vec(0, 1)
-val UP_LEFT = UP + LEFT
-val UP_RIGHT = UP + RIGHT
-val DOWN_LEFT = DOWN + LEFT
-val DOWN_RIGHT = DOWN + RIGHT
