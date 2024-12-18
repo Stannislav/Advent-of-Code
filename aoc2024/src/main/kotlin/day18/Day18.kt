7de5d5f85b1211bac4ace2d33f9fbc3b0de5fe76
Stanislav Schmidt <Stannislav@users.noreply.github.com>
2024-12-18 21:50:39 +0100

    Use Dijkstra instead of BFS

--- aoc2024/src/main/kotlin/day18/Day18.kt
+++ aoc2024/src/main/kotlin/day18/Day18.kt
@@ -21,45 +21,46 @@ fun parseInput(stream: InputStream): List<Vec> {
 }
 
 fun part1(allBytes: List<Vec>, target: Vec, max: Int): Int {
-    return getPath(allBytes.subList(0, max), target)?.size ?: error("Could not find $target")
+    return dijkstra(allBytes.subList(0, max), target)?.size ?: error("Could not find $target")
 }
 
 fun part2(allBytes: List<Vec>, target: Vec, min: Int): String {
     val bytes = allBytes.subList(0, min).toMutableList()
-    var path = getPath(bytes, target)?.toSet() ?: error("Can't find minimal path")
+    var path = dijkstra(bytes, target)?.toSet() ?: error("Can't find minimal path")
     for (byte in allBytes.drop(min)) {
         bytes += byte
         if (path.contains(byte))
-            path = getPath(bytes, target)?.toSet() ?: return byte.run { "${i},${j}" }
+            path = dijkstra(bytes, target)?.toSet() ?: return byte.run { "${i},${j}" }
     }
     error("No solution found")
 }
 
-fun getPath(bytes: List<Vec>, target: Vec): List<Vec>? {
+fun dijkstra(bytes: List<Vec>, target: Vec): List<Vec>? {
     val corrupted = bytes.toSet()
-    val dist = mutableMapOf(Vec(0, 0) to 0)
-    val q: Queue<Vec> = LinkedList()
-    val done = mutableSetOf<Vec>()
-    q.add(Vec(0, 0))
-    val prev = mutableMapOf<Vec, Vec>()
+    val q = PriorityQueue<Pair<Vec, Int>>(compareBy { it.second }) // (pos, dist)
+    q.add(Pair(Vec(0, 0), 0))
+    val prev = mutableMapOf<Vec, Pair<Vec, Int>>() // pos -> (prevPos, prevDist)
     while (q.isNotEmpty()) {
-        val pos = q.remove()
-        done.add(pos)
-        val d = dist[pos]?.let { it + 1 } ?: error("distance missing for $pos")
+        val (pos, dist) = q.remove()
+        if (pos == target)
+            break
         sequenceOf(LEFT, RIGHT, UP, DOWN)
             .map { pos + it }
             .filter { it.i >= 0 && it.j >= 0 && it.i <= target.i && it.j <= target.j }
-            .filterNot { corrupted.contains(it) || done.contains(it) }
-            .filter { dist[it]?.let { oldD -> oldD > d } ?: true }
-            .forEach { dist[it] = d; prev[it] = pos; q.add(it) }
+            .filterNot { corrupted.contains(it) }
+            .filter { prev[it]?.run { dist < second } ?: true }
+            .forEach {
+                prev[it] = Pair(pos, dist)
+                q.add(Pair(it, dist + 1))
+            }
     }
-    if (!dist.contains(target))
-        return null
+
+    // Reconstruct path
     val path = mutableListOf<Vec>()
     var pos = target
     while (pos != Vec(0, 0)) {
         path.add(pos)
-        pos = prev[pos] ?: error("Path incomplete")
+        pos = prev[pos]?.first ?: return null
     }
     return path.reversed()
 }
