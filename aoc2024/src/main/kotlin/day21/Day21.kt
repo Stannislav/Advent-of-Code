406e81c3d0287a84b620dfaff26b8f85e18ad853
Stanislav Schmidt <Stannislav@users.noreply.github.com>
2024-12-30 00:05:32 +0100

    Clean up code

--- aoc2024/src/main/kotlin/day21/Day21.kt
+++ aoc2024/src/main/kotlin/day21/Day21.kt
@@ -4,7 +4,6 @@ import common.*
 import java.io.File
 import java.io.InputStream
 import java.util.*
-import kotlin.math.abs
 
 fun main() {
     val input = parseInput(File("input/21.txt").inputStream())
@@ -12,9 +11,9 @@ fun main() {
     println("Part 2: ${part2(input)}")
 }
 
+fun parseInput(stream: InputStream): List<String> = stream.bufferedReader().readLines()
 fun part1(input: List<String>): Long = solve(input, 2)
 fun part2(input: List<String>): Long = solve(input, 25)
-
 
 fun solve(input: List<String>, nKeypads: Int): Long {
     val arrowKeypad = Keypad.fromString(" ^A\n<v>")
@@ -24,11 +23,7 @@ fun solve(input: List<String>, nKeypads: Int): Long {
     return input.sumOf { line -> line.substring(0, 3).toInt() * line.sumOf(robot::pressButton) }
 }
 
-fun parseInput(stream: InputStream): List<String> {
-    return stream.bufferedReader().readLines()
-}
-
-class Keypad private constructor(val positions: Map<Char, Vec>) {
+class Keypad private constructor(private val positions: Map<Char, Vec>) {
     companion object {
         fun fromString(layout: String): Keypad {
             val positions = layout
@@ -43,10 +38,10 @@ class Keypad private constructor(val positions: Map<Char, Vec>) {
     private val pathCache = mutableMapOf<Pair<Char, Char>, List<List<Char>>>()
 
     fun getAllPaths(from: Char, to: Char): List<List<Char>> = pathCache.getOrPut(Pair(from, to)) {
-        // flood map with dfs
+        // Flood map with BFS.
         val fromPos = positions[from] ?: error("position of from=$from is unknown")
         val toPos = positions[to] ?: error("position of to=$to is unknown")
-        val valid = positions.values.toSet()
+        val validPositions = positions.values.toSet()
         val distances = mutableMapOf(fromPos to 0)
         val q: Queue<Vec> = LinkedList()
         q.add(fromPos)
@@ -54,7 +49,7 @@ class Keypad private constructor(val positions: Map<Char, Vec>) {
             val pos = q.remove()
             sequenceOf(UP, DOWN, LEFT, RIGHT)
                 .map { pos + it }
-                .filter { valid.contains(it) }
+                .filter { validPositions.contains(it) }
                 .filterNot { distances.contains(it) }
                 .forEach { newPos ->
                     distances[newPos] = distances[pos]?.let { it + 1 } ?: error("unknown distance for $pos")
@@ -63,18 +58,19 @@ class Keypad private constructor(val positions: Map<Char, Vec>) {
 
         }
 
+        // Given node distances computed via BFS above, build all possible paths.
         fun buildPaths(currentPos: Vec): List<List<Vec>> {
-            if (currentPos == toPos) {
+            if (currentPos == toPos)
                 return listOf(listOf(currentPos))
-            }
+            val currentDist = distances[currentPos] ?: error("unknown distance for $currentPos")
             return sequenceOf(UP, DOWN, LEFT, RIGHT)
                 .map { currentPos + it }
-                .filter { valid.contains(it) }
-                .filter { distances[it]!! == distances[currentPos]!! + 1 }
+                .filter { distances[it]?.let { dist -> dist == currentDist + 1 } ?: false }
                 .flatMap { buildPaths(it).map { path -> listOf(currentPos) + path } }
                 .toList()
         }
 
+        // Map path positions to direction characters.
         buildPaths(fromPos)
             .map { path ->
                 path.zip(path.drop(1)).map {
@@ -105,7 +101,7 @@ class Robot(private val targetKeypad: Keypad, private val controller: Agent) : A
     override fun pressButton(button: Char): Long = cache.getOrPut(Pair(position, button)) {
         targetKeypad.getAllPaths(position, button)
             .map { it + 'A' }
-            .minOf { it.sumOf { c -> controller.pressButton(c) } }
+            .minOf { it.sumOf { button -> controller.pressButton(button) } }
     }
     .also { position = button }
 }
