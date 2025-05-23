55584b9f647b9fd6a0259a6b4eec1881fc9edab8
Stanislv Schmidt <Stannislav@users.noreply.github.com>
2025-05-23 14:38:54 +0200

    Implement day 14 input parsing

--- 2022/src/main/scala/day14/Solution.scala
+++ 2022/src/main/scala/day14/Solution.scala
@@ -1,14 +1,26 @@
 package day14
 
+import Math.{min, max}
+
 object Solution {
   def main(args: Array[String]): Unit = {
-    val input = parseInput(io.Source.fromResource("input/14.txt"))
+    val rocks = parseInput(io.Source.fromResource("input/14.txt"))
+    println(rocks)
+    println(rocks.size)
+    val maxY = rocks.map(_.y).max
+    println(s"Max y: $maxY")
   }
 
   def parseInput(stream: io.Source): Set[Point]= {
 
-    def fillLines(corners: Array[Point]): Set[Point] = {
-      Set(Point(0, 0))
+    def fillLine(p1: Point, p2: Point): Seq[Point] = {
+      if (p1.x == p2.x) {
+        for(y <- min(p1.y, p2.y) to max(p1.y, p2.y)) yield Point(p1.x, y)
+      } else if(p2.y == p2.y) {
+        for(x <- min(p1.x, p2.x) to max(p1.x, p2.x)) yield Point(x, p1.y)
+      } else {
+        throw Exception(s"Invalid line corners: $p1, $p2")
+      }
     }
 
     stream
@@ -18,7 +30,8 @@ object Solution {
         case Array(x: Int, y: Int) => Point(x, y)
         case invalid@_ => throw Exception(s"Invalid point: ${invalid.mkString(",")}")
       })
-      .map(fillLines)
+      .flatMap(corners => (corners zip corners.tail).map(fillLine))
+      .map(_.toSet)
       .reduce(_ | _)
   }
 }
