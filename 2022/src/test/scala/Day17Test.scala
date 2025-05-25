5b9705e936e8cb2939b4eae08f5b67046fbd000b
Stanislv Schmidt <Stannislav@users.noreply.github.com>
2025-05-25 21:16:58 +0200

    Add tests for part 1 and part 2

--- 2022/src/test/scala/Day17Test.scala
+++ 2022/src/test/scala/Day17Test.scala
@@ -1,12 +1,26 @@
 import org.scalatest.funsuite.AnyFunSuite
-
-import day17.Solution
+import io.Source
+import day17.{ChamberSimulation, Solution}
 
 class Day17Test extends AnyFunSuite {
   private val input = ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>\n"
 
   test("Solution.parseInput") {
     val expected = ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>"
-    assert(Solution.parseInput(io.Source.fromString(input)) === expected)
+    assert(Solution.parseInput(Source.fromString(input)) === expected)
+  }
+
+  test("Solution.part1") {
+    val jets = Solution.parseInput(Source.fromString(input))
+    val (heights, loopStart, loopHeightGain) = ChamberSimulation.run(jets)
+    val solution = Solution(heights, loopStart, loopHeightGain)
+    assert(solution.part1 === 3068)
+  }
+
+  test("Solution.part2") {
+    val jets = Solution.parseInput(Source.fromString(input))
+    val (heights, loopStart, loopHeightGain) = ChamberSimulation.run(jets)
+    val solution = Solution(heights, loopStart, loopHeightGain)
+    assert(solution.part2 === 1514285714288L)
   }
 }
