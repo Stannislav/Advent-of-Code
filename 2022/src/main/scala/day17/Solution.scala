2bd6bb6e734fdacca2c52715f4bfc22b29f0de09
Stanislv Schmidt <Stannislav@users.noreply.github.com>
2025-05-25 19:37:17 +0200

    Factor out Solution class

--- 2022/src/main/scala/day17/Solution.scala
+++ 2022/src/main/scala/day17/Solution.scala
@@ -9,19 +9,25 @@ object Solution {
   def main(args: Array[String]): Unit = {
     val jets = parseInput(Source.fromResource("input/17.txt"))
     val (heights, loopStart, loopHeightGain) = ChamberSimulation.run(jets)
-    val loopLength = heights.length - loopStart
-
-    def getHeightAfter(nSteps: Long): Long = {
-      if (nSteps < heights.length) {
-        heights(nSteps.toInt)
-      } else {
-        val nLoops = (nSteps - loopStart) / loopLength
-        val idxInLoop = (nSteps - loopStart) % loopLength
-        heights(loopStart + idxInLoop.toInt) + nLoops * loopHeightGain
-      }
-    }
-
-    println(s"Part 1: ${getHeightAfter(2022)}")
-    println(s"Part 2: ${getHeightAfter(1000000000000L)}")
+    val solution = Solution(heights, loopStart, loopHeightGain)
+    println(s"Part 1: ${solution.part1}")
+    println(s"Part 2: ${solution.part2}")
   }
+}
+
+class Solution(heights: Array[Int], loopStart: Int, loopHeightGain: Int) {
+  private val loopLength = heights.length - loopStart
+
+  private def getHeightAfter(nSteps: Long): Long = {
+    if (nSteps < heights.length) {
+      heights(nSteps.toInt)
+    } else {
+      val nLoops = (nSteps - loopStart) / loopLength
+      val idxInLoop = (nSteps - loopStart) % loopLength
+      heights(loopStart + idxInLoop.toInt) + nLoops * loopHeightGain
+    }
+  }
+
+  def part1: Long = getHeightAfter(2022)
+  def part2: Long = getHeightAfter(1000000000000L)
 }
