dcbcbed5d82ab79b5db340bc0944e2e86ec432fe
Stanislv Schmidt <Stannislav@users.noreply.github.com>
2025-05-25 21:16:43 +0200

    Use simpler chamber hash based on heuristics

--- 2022/src/main/scala/day17/ChamberSimulation.scala
+++ 2022/src/main/scala/day17/ChamberSimulation.scala
@@ -14,34 +14,33 @@ object ChamberSimulation {
   private var jetIdx = 0
   private var chamber: Array[Int] = Array()
 
+  private def chamberHash(chamber: Array[Int]) = chamber.takeRight(30).toSeq.hashCode()
+
   def run(jets: String): (Array[Int], Int, Int) = {
     rockIdx = 0
     jetIdx = 0
     chamber = Array()
-    // (jetIdx, rockIdx, reducedChamber) => (nSteps, height)
-    val cache = collection.mutable.Map[(Int, Int, Seq[Int]), (Int, Int)]()
-    var prunedHeight = 0
+    // (jetIdx, rockIdx, chamberHash) => (nSteps, height)
+    val cache = collection.mutable.Map[(Int, Int, Int), (Int, Int)]()
     var nSteps = 0
 
     // Simulate rock falling until a loop is detected.
-    while (!cache.contains((jetIdx, rockIdx, chamber))) {
-      cache((jetIdx, rockIdx, chamber)) = (nSteps, chamber.length + prunedHeight)
+    while (!cache.contains((jetIdx, rockIdx, chamberHash(chamber)))) {
+      cache((jetIdx, rockIdx, chamberHash(chamber))) = (nSteps, chamber.length)
       dropRock(ROCK_SHAPES(rockIdx), jets)
       rockIdx = (rockIdx + 1) % ROCK_SHAPES.length
-      prunedHeight += pruneChamber()
       nSteps += 1
     }
 
     // Translate cached chamber states into an array of heights indexed
     // by the step number. Also compute the loop length and the height gain
     // in the loop.
-    val (loopStart, startHeight) = cache((jetIdx, rockIdx, chamber))
+    val (loopStart, startHeight) = cache((jetIdx, rockIdx, chamberHash(chamber)))
     val heights = Array.fill(cache.size)(0)
     for ((idx, height) <- cache.values) {
       heights(idx) = height
     }
-    val heightAfterLoop = chamber.length + prunedHeight
-    val loopHeightGain = heightAfterLoop - heights(loopStart)
+    val loopHeightGain = chamber.length - heights(loopStart)
     (heights, loopStart, loopHeightGain)
   }
 
@@ -65,12 +64,5 @@ object ChamberSimulation {
       }
     }
     chamber = rock.mergeIntoChamber(chamber, pos)
-  }
-
-
-  private def pruneChamber(): Int = {
-    val prunedHeight = chamber.lastIndexOf(FULL_LINE) + 1
-    chamber = chamber.slice(prunedHeight, chamber.length)
-    prunedHeight
   }
 }
