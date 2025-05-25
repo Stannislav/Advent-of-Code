a977aad867da1ce3f07da9f5b09ed25e42ab33e8
Stanislv Schmidt <Stannislav@users.noreply.github.com>
2025-05-25 18:48:50 +0200

    Improve naming

--- 2022/src/main/scala/day17/Rock.scala
+++ 2022/src/main/scala/day17/Rock.scala
@@ -16,40 +16,40 @@ object Rock {
   }
 }
 
-private class Rock(var data: Array[Int]) {
-  private def canMoveRight = data.map(_ & 1).forall(_ == 0)
+private class Rock(var rockData: Array[Int]) {
+  private def canMoveRight = rockData.map(_ & 1).forall(_ == 0)
 
-  private def canMoveLeft = data.map(_ & 64).forall(_ == 0)
+  private def canMoveLeft = rockData.map(_ & 64).forall(_ == 0)
 
-  private def moveRight = data.map(_ >> 1)
+  private def moveRight = rockData.map(_ >> 1)
 
-  private def moveLeft = data.map(_ << 1)
+  private def moveLeft = rockData.map(_ << 1)
 
-  def isCollision(state: Array[Int], rockPos: Int): Boolean = {
-    val ids = (rockPos until rockPos + data.length).filter(_ < state.length)
-    (data zip ids).exists((line, idx) => (line & state(idx)) != 0)
+  def isCollision(chamber: Array[Int], rockPos: Int): Boolean = {
+    val ids = (rockPos until rockPos + rockData.length).filter(_ < chamber.length)
+    (rockData zip ids).exists((line, idx) => (line & chamber(idx)) != 0)
   }
 
-  def maybeMoveLeft(state: Array[Int], rockPos: Int): Unit = {
+  def maybeMoveLeft(chamber: Array[Int], rockPos: Int): Unit = {
     if (canMoveLeft) {
-      data = moveLeft
-      if (isCollision(state, rockPos))
-        data = moveRight
+      rockData = moveLeft
+      if (isCollision(chamber, rockPos))
+        rockData = moveRight
     }
   }
 
-  def maybeMoveRight(state: Array[Int], rockPos: Int): Unit = {
+  def maybeMoveRight(chamber: Array[Int], rockPos: Int): Unit = {
     if (canMoveRight) {
-      data = moveRight
-      if (isCollision(state, rockPos))
-        data = moveLeft
+      rockData = moveRight
+      if (isCollision(chamber, rockPos))
+        rockData = moveLeft
     }
   }
 
-  def mergeIntoState(state: Array[Int], rockPos: Int): Array[Int] = {
-    val newState = state.padTo(rockPos + data.length, 0)
-    val ids = rockPos until rockPos + data.length
-    (data zip ids).foreach((line, idx) => newState(idx) |= line)
+  def mergeIntoChamber(chamber: Array[Int], rockPos: Int): Array[Int] = {
+    val newState = chamber.padTo(rockPos + rockData.length, 0)
+    val ids = rockPos until rockPos + rockData.length
+    (rockData zip ids).foreach((line, idx) => newState(idx) |= line)
     newState
   }
 }
--- 2022/src/main/scala/day17/SolutionNew.scala
+++ 2022/src/main/scala/day17/SolutionNew.scala
@@ -12,59 +12,60 @@ object SolutionNew {
   )
 
   def main(args: Array[String]): Unit = {
-    val ops = parseInput(Source.fromResource("input/17.txt"))
+    val jets = parseInput(Source.fromResource("input/17.txt"))
 
     var rockIdx = 0
-    var opIdx = 0
-    var state = Array[Int]()
+    var jetIdx = 0
+    var chamber = Array[Int]()
 
-    def fall(rockShape: Seq[String], state: Array[Int]): Array[Int] = {
-      var pos = state.length + 3
+    def fall(rockShape: Seq[String], chamber: Array[Int]): Array[Int] = {
+      var pos = chamber.length + 3
       var done = false
       val rock = Rock.fromShape(rockShape)
       while(!done) {
         // 1. Shift left-right
-        ops(opIdx) match {
-          case '<' => rock.maybeMoveLeft(state, pos)
-          case '>' => rock.maybeMoveRight(state, pos)
+        jets(jetIdx) match {
+          case '<' => rock.maybeMoveLeft(chamber, pos)
+          case '>' => rock.maybeMoveRight(chamber, pos)
         }
-        opIdx = (opIdx + 1) % ops.length
+        jetIdx = (jetIdx + 1) % jets.length
 
         // 2. Shift down
         pos -= 1
-        if (pos < 0 || rock.isCollision(state, pos)) {
+        if (pos < 0 || rock.isCollision(chamber, pos)) {
           pos += 1
           done = true
         }
       }
-      rock.mergeIntoState(state, pos)
+      rock.mergeIntoChamber(chamber, pos)
     }
 
+    // (jetIdx, rockIdx, reducedChamber) => (nSteps, height)
     val cache = collection.mutable.Map[(Int, Int, Seq[Int]), (Int, Int)]()
-    def reduceState(state: Array[Int]): (Array[Int], Int) = {
+    def reduceChamber(chamber: Array[Int]): (Array[Int], Int) = {
       val fullLine = 127 // 0b1111111
-      val cutOff = state.lastIndexOf(fullLine) + 1
-      (state.slice(cutOff, state.length), cutOff)
+      val cutOff = chamber.lastIndexOf(fullLine) + 1
+      (chamber.slice(cutOff, chamber.length), cutOff)
     }
 
     var prunedHeight = 0
     var nSteps = 0
-    while(!cache.contains((opIdx, rockIdx, state))) {
-      cache((opIdx, rockIdx, state)) = (nSteps, state.length + prunedHeight)
-      state = fall(ROCK_SHAPES(rockIdx), state)
+    while(!cache.contains((jetIdx, rockIdx, chamber))) {
+      cache((jetIdx, rockIdx, chamber)) = (nSteps, chamber.length + prunedHeight)
+      chamber = fall(ROCK_SHAPES(rockIdx), chamber)
       rockIdx = (rockIdx + 1) % ROCK_SHAPES.length
-      val (reducedState, reducedLines) = reduceState(state)
-      state = reducedState
+      val (reducedChamber, reducedLines) = reduceChamber(chamber)
+      chamber = reducedChamber
       prunedHeight += reducedLines
       nSteps += 1
     }
-    val (loopStart, startHeight) = cache((opIdx, rockIdx, state))
+    val (loopStart, startHeight) = cache((jetIdx, rockIdx, chamber))
     val loopLength = nSteps - loopStart
     val heights = Array.fill(cache.size) { 0 }
     for((idx, height) <- cache.values) {
       heights(idx) = height
     }
-    val loopHeight = state.length + prunedHeight - heights(loopStart)
+    val loopHeight = chamber.length + prunedHeight - heights(loopStart)
 
     def getHeightAfter(steps: Long): Long = {
       if (steps < heights.length) {
