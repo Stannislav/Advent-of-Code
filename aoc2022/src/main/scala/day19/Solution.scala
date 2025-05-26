8a147b4ef1017f07f158747f5b6fc4d164ea24bf
Stanislv Schmidt <Stannislav@users.noreply.github.com>
2025-05-26 15:45:42 +0200

    Add new day 19 parsing and tests

--- aoc2022/src/main/scala/day19/Solution.scala
+++ aoc2022/src/main/scala/day19/Solution.scala
@@ -1,11 +1,33 @@
 // TODO: Finish solving.
 package day19
 
+import io.Source
+
 object Solution {
   def main(args: Array[String]): Unit = {
-    val input = util.Using.resource(io.Source.fromResource("input/19-debug.txt")) {
-      _.getLines.map(Blueprint.fromString).toList
+//    val input = util.Using.resource(io.Source.fromResource("input/19-debug.txt")) {
+//      _.getLines.map(Blueprint.fromString).toList
+//    }
+//    println(input.map(_.run()))
+  }
+
+  def parseInput(stream: Source): Seq[Array[Array[Int]]] = {
+    stream
+      .getLines()
+      .map(parseBlueprint)
+      .toSeq
+  }
+
+  private def parseBlueprint(line: String): Array[Array[Int]] = {
+    line.match {
+      case s"Blueprint $i: Each ore robot costs $oreOre ore. Each clay robot costs $clayOre ore. Each obsidian robot costs $obsidianOre ore and $obsidianClay clay. Each geode robot costs $geodeOre ore and $geodeObsidian obsidian." =>
+        Array(
+          Array(oreOre.toInt, 0, 0, 0),
+          Array(clayOre.toInt, 0, 0, 0),
+          Array(obsidianOre.toInt, obsidianClay.toInt, 0, 0),
+          Array(geodeOre.toInt, 0, geodeObsidian.toInt, 0),
+        )
+      case _ => throw Exception(s"Can't parse line: $line")
     }
-    println(input.map(_.run()))
   }
 }
---
+++ aoc2022/src/test/scala/Day19Test.scala
@@ -0,0 +1,30 @@
+import org.scalatest.funsuite.AnyFunSuite
+import io.Source
+import day19.Solution
+
+class Day19Test extends AnyFunSuite {
+  //Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.
+  //Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.
+  test("Solution.parseInput") {
+    val expected = Seq(
+      Array(
+        Array(4, 0, 0, 0),
+        Array(2, 0, 0, 0),
+        Array(3, 14, 0, 0),
+        Array(2, 0, 7, 0),
+      ),
+      Array(
+        Array(2, 0, 0, 0),
+        Array(3, 0, 0, 0),
+        Array(3, 8, 0, 0),
+        Array(3, 0, 12, 0),
+      )
+    )
+    val parsed = Solution.parseInput(Source.fromResource("19.txt"))
+    assert(parsed.length === expected.length)
+    (parsed zip expected).foreach { (a, b) => assert(a.length === b.length)}
+    (parsed zip expected).foreach { (a, b) =>
+      (a zip b).foreach { (x, y) => assert(x.toSeq === y.toSeq)}
+    }
+  }
+}
