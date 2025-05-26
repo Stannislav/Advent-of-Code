3ac0392b39fdca37bf592ea10ca803f931fc04e1
Stanislv Schmidt <Stannislav@users.noreply.github.com>
2025-05-26 07:50:31 +0200

    Move blueprint parsing to Blueprint object

--- aoc2022/src/main/scala/day19/Blueprint.scala
+++ aoc2022/src/main/scala/day19/Blueprint.scala
@@ -7,7 +7,24 @@ val CLAY = 1
 val OBSIDIAN = 2
 val GEODE = 3
 val NAMES = List("ore", "clay", "obsidian", "geode")
-class Blueprint(i: Int, robotCosts: List[List[Int]]) {
+
+object Blueprint {
+  def fromString(line: String): Blueprint = {
+    line.match {
+      case s"Blueprint $i: Each ore robot costs $oreOre ore. Each clay robot costs $clayOre ore. Each obsidian robot costs $obsidianOre ore and $obsidianClay clay. Each geode robot costs $geodeOre ore and $geodeObsidian obsidian." =>
+        new Blueprint(
+          i.toInt,
+          List(
+            List(oreOre.toInt, 0, 0, 0),
+            List(clayOre.toInt, 0, 0, 0),
+            List(obsidianOre.toInt, obsidianClay.toInt, 0, 0),
+            List(geodeOre.toInt, 0, geodeObsidian.toInt, 0),
+          )
+        )
+    }
+  }
+}
+class Blueprint private(i: Int, robotCosts: List[List[Int]]) {
 
   private def update(resources: List[Int], robots: List[Int], useResources: List[Int], buildRobots: List[Int]): (List[Int], List[Int]) = {
     val newResources = Range(0, 4).map(kind => resources(kind) - useResources(kind) + robots(kind)).toList
--- aoc2022/src/main/scala/day19/Solution.scala
+++ aoc2022/src/main/scala/day19/Solution.scala
@@ -4,23 +4,8 @@ package day19
 object Solution {
   def main(args: Array[String]): Unit = {
     val input = util.Using.resource(io.Source.fromResource("input/19-debug.txt")) {
-      _.getLines.map(parseBlueprint).toList
+      _.getLines.map(Blueprint.fromString).toList
     }
     println(input.map(_.run()))
-  }
-
-  private def parseBlueprint(line: String): Blueprint = {
-    line.match {
-      case s"Blueprint $i: Each ore robot costs $oreOre ore. Each clay robot costs $clayOre ore. Each obsidian robot costs $obsidianOre ore and $obsidianClay clay. Each geode robot costs $geodeOre ore and $geodeObsidian obsidian." =>
-        new Blueprint(
-          i.toInt,
-          List(
-            List(oreOre.toInt, 0, 0, 0),
-            List(clayOre.toInt, 0, 0, 0),
-            List(obsidianOre.toInt, obsidianClay.toInt, 0, 0),
-            List(geodeOre.toInt, 0, geodeObsidian.toInt, 0),
-          )
-        )
-    }
   }
 }
