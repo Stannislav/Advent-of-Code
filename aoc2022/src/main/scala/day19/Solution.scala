88144ea4d5b6bad20a5458fae7603f1a3a81c753
Stanislv Schmidt <Stannislav@users.noreply.github.com>
2025-05-26 16:28:54 +0200

    Replace Array by Vector

--- aoc2022/src/main/scala/day19/Solution.scala
+++ aoc2022/src/main/scala/day19/Solution.scala
@@ -11,34 +11,34 @@ object Solution {
 //    println(input.map(_.run()))
   }
 
-  def parseInput(stream: Source): Seq[Array[Array[Int]]] = {
+  def parseInput(stream: Source): Seq[Vector[Vector[Int]]] = {
     stream
       .getLines()
       .map(parseBlueprint)
-      .toSeq
+      .toVector
   }
 
-  private def parseBlueprint(line: String): Array[Array[Int]] = {
+  private def parseBlueprint(line: String): Vector[Vector[Int]] = {
     line.match {
       case s"Blueprint $i: Each ore robot costs $oreOre ore. Each clay robot costs $clayOre ore. Each obsidian robot costs $obsidianOre ore and $obsidianClay clay. Each geode robot costs $geodeOre ore and $geodeObsidian obsidian." =>
-        Array(
-          Array(oreOre.toInt, 0, 0, 0),
-          Array(clayOre.toInt, 0, 0, 0),
-          Array(obsidianOre.toInt, obsidianClay.toInt, 0, 0),
-          Array(geodeOre.toInt, 0, geodeObsidian.toInt, 0),
+        Vector(
+          Vector(oreOre.toInt, 0, 0, 0),
+          Vector(clayOre.toInt, 0, 0, 0),
+          Vector(obsidianOre.toInt, obsidianClay.toInt, 0, 0),
+          Vector(geodeOre.toInt, 0, geodeObsidian.toInt, 0),
         )
       case _ => throw Exception(s"Can't parse line: $line")
     }
   }
 
-  def part1(blueprints: Seq[Array[Array[Int]]]): Int = {
+  def part1(blueprints: Seq[Vector[Vector[Int]]]): Int = {
     blueprints
       .zipWithIndex
       .map((blueprint, idx) => (idx + 1) * runOptimally(blueprint))
       .sum
   }
 
-  def runOptimally(blueprint: Array[Array[Int]]): Int = {
+  def runOptimally(blueprint: Vector[Vector[Int]]): Int = {
     0
   }
 }
--- aoc2022/src/test/scala/Day19Test.scala
+++ aoc2022/src/test/scala/Day19Test.scala
@@ -7,25 +7,20 @@ class Day19Test extends AnyFunSuite {
   //Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.
   test("Solution.parseInput") {
     val expected = Seq(
-      Array(
-        Array(4, 0, 0, 0),
-        Array(2, 0, 0, 0),
-        Array(3, 14, 0, 0),
-        Array(2, 0, 7, 0),
+      Vector(
+        Vector(4, 0, 0, 0),
+        Vector(2, 0, 0, 0),
+        Vector(3, 14, 0, 0),
+        Vector(2, 0, 7, 0),
       ),
-      Array(
-        Array(2, 0, 0, 0),
-        Array(3, 0, 0, 0),
-        Array(3, 8, 0, 0),
-        Array(3, 0, 12, 0),
+      Vector(
+        Vector(2, 0, 0, 0),
+        Vector(3, 0, 0, 0),
+        Vector(3, 8, 0, 0),
+        Vector(3, 0, 12, 0),
       )
     )
-    val parsed = Solution.parseInput(Source.fromResource("19.txt"))
-    assert(parsed.length === expected.length)
-    (parsed zip expected).foreach { (a, b) => assert(a.length === b.length)}
-    (parsed zip expected).foreach { (a, b) =>
-      (a zip b).foreach { (x, y) => assert(x.toSeq === y.toSeq)}
-    }
+    assert(Solution.parseInput(Source.fromResource("19.txt")) === expected)
   }
 
   test("Solution.runOptimally") {
