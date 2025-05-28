5a2c63502060a1db0bf8d13df990f7f8c890b7e3
Stanislv Schmidt <Stannislav@users.noreply.github.com>
2025-05-28 09:50:51 +0200

    Implement brute-force day 19 solution

--- aoc2022/src/main/scala/day19/Solution.scala
+++ aoc2022/src/main/scala/day19/Solution.scala
@@ -39,6 +39,51 @@ object Solution {
   }
 
   def runOptimally(blueprint: Vector[Vector[Int]]): Int = {
-    0
+    var state = Set((Vector(1, 0, 0, 0), Vector(0, 0, 0, 0)))
+    val seen = collection.mutable.Set[(Vector[Int], Vector[Int])]()
+
+    def step(nRobots: Vector[Int], nResources: Vector[Int]): Seq[(Vector[Int], Vector[Int])] = {
+      Seq(
+        (Vector(0, 0, 0, 0), nResources),
+        (Vector(1, 0, 0, 0), (nResources zip blueprint(0)).map(_ - _)),
+        (Vector(0, 1, 0, 0), (nResources zip blueprint(1)).map(_ - _)),
+        (Vector(0, 0, 1, 0), (nResources zip blueprint(2)).map(_ - _)),
+        (Vector(0, 0, 0, 1), (nResources zip blueprint(3)).map(_ - _)),
+      )
+        .filter(_._2.forall {_ >= 0})
+        .map { (robotBuilds, resources) =>
+          ((nRobots zip robotBuilds).map(_ + _), (resources zip nRobots).map(_ + _))
+        }
+        .filter(!seen.contains(_))
+    }
+
+    for(i <- 1 to 24) {
+//      println(s"Current length: ${state.size}")
+//      println(s"  $state")
+      val nextState = state.flatMap { (nRobots, nResources) => step(nRobots, nResources) }
+      nextState.foreach(seen.add)
+      // for the same resources pick the state with the best robots state
+      state = nextState
+        .groupBy(_._2)
+        .values
+        .map { items =>
+          if (items.exists(_._1(3) != 0)) {
+            val max = items.map(_._1(3)).max
+            items.filter(_._1(3) == max)
+          } else if (items.exists(_._1(2) != 0)) {
+            val max = items.map(_._1(2)).max
+            items.filter(_._1(2) == max)
+          } else if (items.exists(_._1(1) != 0)) {
+            val max = items.map(_._1(1)).max
+            items.filter(_._1(1) == max)
+          } else {
+            val max = items.map(_._1(0)).max
+            items.filter(_._1(0) == max)
+          }
+        }
+        .reduce(_ | _)
+    }
+    println(s"Total states at the end: ${state.size}")
+    state.map(_._2(3)).max
   }
 }
