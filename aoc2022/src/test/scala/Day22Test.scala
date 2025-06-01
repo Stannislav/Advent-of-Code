eb074e528b19c288963a57e975554fb3f99294d2
Stanislv Schmidt <Stannislav@users.noreply.github.com>
2025-06-01 20:54:16 +0200

    Use resource file in tests

--- aoc2022/src/test/scala/Day22Test.scala
+++ aoc2022/src/test/scala/Day22Test.scala
@@ -4,25 +4,8 @@ import org.scalatest.funsuite.AnyFunSuite
 import io.Source
 
 class Day22Test extends AnyFunSuite {
-  private val input =
-  """        ...#
-    |        .#..
-    |        #...
-    |        ....
-    |...#.......#
-    |........#...
-    |..#....#....
-    |..........#.
-    |        ...#....
-    |        .....#..
-    |        .#......
-    |        ......#.
-    |
-    |10R5L5R10L4R5L5
-    |""".stripMargin
-
   test("Solution.parseInput") {
-    val (map, path) = Solution.parseInput(Source.fromString(input))
+    val (map, path) = Solution.parseInput(Source.fromResource("22.txt"))
     val expectedPath = List[Matchable](10, 'R', 5, 'L', 5, 'R', 10, 'L', 4, 'R', 5, 'L', 5)
     assert(map.size === 96)
     assert(map.values.toSet == Set('.', '#'))
@@ -30,12 +13,12 @@ class Day22Test extends AnyFunSuite {
   }
 
   test("Solution.part1") {
-    val (map, path) = Solution.parseInput(Source.fromString(input))
+    val (map, path) = Solution.parseInput(Source.fromResource("22.txt"))
     assert(Solution.part1(map, path) === 6032)
   }
 
   test("Solution.part2") {
-    val (map, path) = Solution.parseInput(Source.fromString(input))
+    val (map, path) = Solution.parseInput(Source.fromResource("22.txt"))
     assert(Solution.part2(map, path) === 5031)
   }
 }
