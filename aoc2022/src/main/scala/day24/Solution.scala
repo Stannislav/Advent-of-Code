acd27caf8170b301c0184262d744826ff4303147
Stanislv Schmidt <Stannislav@users.noreply.github.com>
2025-06-03 11:54:01 +0200

    Add stub for part 2

--- aoc2022/src/main/scala/day24/Solution.scala
+++ aoc2022/src/main/scala/day24/Solution.scala
@@ -9,17 +9,23 @@ object Solution {
   def main(args: Array[String]): Unit = {
     val (start, end, blizzards) = parseInput(Source.fromResource("input/24.txt"))
 
-    println(s"Part1: ${part1(start, end, blizzards)}")
+    println(s"Part 1: ${part1(start, end, blizzards)}")
+    println(s"Part 2: ${part1(start, end, blizzards)}")
   }
 
-  def part1(start: (Int, Int), end: (Int, Int), blizzards: Blizzards): Int = {
+  def part1(start: (Int, Int), end: (Int, Int), blizzards: Blizzards): Int = travel(start, end, blizzards)
+
+  def part2(start: (Int, Int), end: (Int, Int), blizzards: Blizzards): Int = {
+    0
+  }
+
+  private def travel(start: (Int, Int), end: (Int, Int), blizzards: Blizzards, startTime: Int = 0): Int = {
     val best = List.fill(blizzards.cycleLength) { mutable.Map[(Int, Int), Int]() }
-    best.head(start) = 0
-    val q = mutable.Queue((start, 0))
+    best.head(start) = startTime
+    val q = mutable.Queue((start, startTime))
     while(q.nonEmpty) {
       val (pos, prevTime) = q.dequeue()
       val time = prevTime + 1
-//      println(s"t=$time")
       val cycleTime = time % blizzards.cycleLength
       val currentBlizzards = blizzards.atTime(time)
 
@@ -33,7 +39,7 @@ object Solution {
           q.enqueue((nextPos, time))
         }
     }
-    best.filter(_.contains(end)).map(_(end)).min
+    best.filter(_.contains(end)).map(_(end)).min - startTime
   }
 
   def parseInput(source: Source): ((Int, Int), (Int, Int), Blizzards) = {
