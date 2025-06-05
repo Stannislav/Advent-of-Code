36aa32e3e85abf2538acdc86d0983b526d33682c
Stanislv Schmidt <Stannislav@users.noreply.github.com>
2025-06-05 08:06:13 +0200

    Solve part 2

--- aoc2023/cmd/day20/main.go
+++ aoc2023/cmd/day20/main.go
@@ -126,5 +126,95 @@ func pressButton(modules map[string]Module, connections map[string][]string) []S
 }
 
 func part2(modules map[string]Module, connections map[string][]string) int {
-	return 0
+	rxInputs := findInputsOf("rx", connections)
+	if len(rxInputs) != 1 {
+		panic("Expected exactly one input to rx, found: " + strings.Join(rxInputs, ", "))
+	}
+	ensureModuleIsConjunction(rxInputs[0], modules)
+
+	rxFirstParents := findInputsOf(rxInputs[0], connections)
+	for _, input := range rxFirstParents {
+		ensureModuleIsConjunction(input, modules)
+	}
+	rxSecondsParents := []string{}
+	for _, mod := range rxFirstParents {
+		parents := findInputsOf(mod, connections)
+		if len(parents) != 1 {
+			panic("Expected exactly one input to " + mod + ", found: " + strings.Join(parents, ", "))
+		}
+		ensureModuleIsConjunction(parents[0], modules)
+		rxSecondsParents = append(rxSecondsParents, parents[0])
+	}
+
+	modToCycle := make(map[string]int)
+	for _, mod := range rxSecondsParents {
+		modToCycle[mod] = -1 // -1 means not yet set
+	}
+
+	buttonPressCounts := 0
+	for done := false; !done; {
+		signals := pressButton(modules, connections)
+		buttonPressCounts++
+
+		// Check if any of the cycles have been found.
+		for _, signal := range signals {
+			for mod := range modToCycle {
+				if signal.from == mod && !signal.value {
+					modToCycle[mod] = buttonPressCounts
+				}
+			}
+		}
+
+		// Have all cycles been found?
+		done = true
+		for _, cycle := range modToCycle {
+			if cycle == -1 {
+				done = false
+				break
+			}
+		}
+	}
+
+	cycles := []int{}
+	for _, cycle := range modToCycle {
+		cycles = append(cycles, cycle)
+	}
+	return LCM(cycles[0], cycles[1], cycles[2:]...)
+}
+
+func findInputsOf(moduleName string, connections map[string][]string) []string {
+	inputs := []string{}
+	for from, to := range connections {
+		if slices.Contains(to, moduleName) {
+			inputs = append(inputs, from)
+		}
+	}
+	return inputs
+}
+
+func ensureModuleIsConjunction(moduleName string, modules map[string]Module) {
+	if _, ok := modules[moduleName].(*Conjunction); !ok {
+		panic("Expected " + moduleName + " to be a Conjunction module")
+	}
+}
+
+// greatest common divisor (GCD) via Euclidean algorithm
+func GCD(a, b int) int {
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
+	}
+	return a
+}
+
+// find Least Common Multiple (LCM) via GCD
+func LCM(a, b int, integers ...int) int {
+	result := a * b / GCD(a, b)
+
+	for i := 0; i < len(integers); i++ {
+		result = LCM(result, integers[i])
+	}
+
+	return result
 }
