0538a92008ff44290e873388dd3c48bac11a07d0
Stanislv Schmidt <Stannislav@users.noreply.github.com>
2025-06-10 14:16:18 +0200

    Refactor function into different files

--- aoc2023/cmd/day21/main.go
+++ aoc2023/cmd/day21/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"container/list"
 	"fmt"
 	"image"
 	"os"
@@ -12,6 +11,48 @@ func main() {
 	start, m := ParseInput("input/21.txt")
 	fmt.Println("Part 1:", Part1(start, &m))
 	fmt.Println("Part 2:", Part2(start, &m))
+}
+
+func ParseInput(filename string) (image.Point, Map) {
+	inputBytes, _ := os.ReadFile(filename)
+	input := strings.TrimSpace(string(inputBytes))
+
+	start := image.Point{-1, -1}
+	var plots [][]rune
+	rocks := make(map[image.Point]bool)
+	for x, line := range strings.Split(input, "\n") {
+		var row []rune
+		for y, c := range line {
+			if c == 'S' {
+				start = image.Point{x, y}
+				c = '.'
+			}
+			row = append(row, c)
+			if c == '#' {
+				rocks[image.Point{x, y}] = true
+			}
+		}
+		plots = append(plots, row)
+	}
+	m := Map{rocks: rocks, lim: image.Point{len(plots), len(plots[0])}}
+	return start, m
+}
+
+func Part1(start image.Point, m *Map) int {
+	return bruteForce(start, m, 64)
+}
+
+func bruteForce(start image.Point, m *Map, target int) int {
+	distances := m.walk(start, target)
+
+	solution := 0
+	for _, steps := range distances {
+		if steps%2 == target%2 {
+			solution++
+		}
+	}
+
+	return solution
 }
 
 func Part2(start image.Point, m *Map) int {
@@ -116,10 +157,6 @@ func ReachablePointsByTiles(n int, start image.Point, m *Map) int {
 	return n*n*W + (2*n+1)*W_o - (n+1)*x_o + n*x_e
 }
 
-func dist(pt1, pt2 image.Point) int {
-	return abs(pt1.X-pt2.X) + abs(pt1.Y-pt2.Y)
-}
-
 func Part2Debug(start image.Point, m *Map) int {
 
 	fmt.Printf("Map size is %v\n", m.lim)
@@ -206,7 +243,7 @@ func Part2Debug(start image.Point, m *Map) int {
 	oddCoords := []image.Point{}
 	for x := 0; x < m.lim.X; x++ {
 		for y := 0; y < m.lim.Y; y++ {
-			pt := image.Point{X: x, Y: y}
+			pt := image.Point{x, y}
 			if _, ok := m.rocks[pt]; !ok {
 				if (x+y)%2 == 0 {
 					evenCoords = append(evenCoords, pt)
@@ -293,7 +330,7 @@ func Part2Debug(start image.Point, m *Map) int {
 	// fmt.Printf("Border points: %v (%d)\n", borderPoints, len(borderPoints))
 	// for x := 0; x < m.lim.X; x++ {
 	// 	for y := 0; y < m.lim.Y; y++ {
-	// 		if _, ok := dist[image.Point{X: x, Y: y}]; ok {
+	// 		if _, ok := dist[image.Point{x, y}]; ok {
 	// 			fmt.Print("#")
 	// 		} else {
 	// 			fmt.Print(".")
@@ -304,31 +341,6 @@ func Part2Debug(start image.Point, m *Map) int {
 	nn := 202300
 	// N_n = n^2W + 2(n+1)W_o - (n+1)x_o + nx_e
 	return nn*nn*w + (2*nn+1)*cube - (nn+1)*xx + nn*xxPrime
-}
-
-func ParseInput(filename string) (image.Point, Map) {
-	inputBytes, _ := os.ReadFile(filename)
-	input := strings.TrimSpace(string(inputBytes))
-
-	start := image.Point{X: -1, Y: -1}
-	var plots [][]rune
-	rocks := make(map[image.Point]bool)
-	for x, line := range strings.Split(input, "\n") {
-		var row []rune
-		for y, c := range line {
-			if c == 'S' {
-				start = image.Point{X: x, Y: y}
-				c = '.'
-			}
-			row = append(row, c)
-			if c == '#' {
-				rocks[image.Point{X: x, Y: y}] = true
-			}
-		}
-		plots = append(plots, row)
-	}
-	m := Map{rocks: rocks, lim: image.Point{X: len(plots), Y: len(plots[0])}}
-	return start, m
 }
 
 func filterEven(distances map[image.Point]int) map[image.Point]int {
@@ -349,127 +361,4 @@ func filterOdd(distances map[image.Point]int) map[image.Point]int {
 		}
 	}
 	return result
-}
-
-func Part1(start image.Point, m *Map) int {
-	return bruteForce(start, m, 64)
-}
-
-func bruteForce(start image.Point, m *Map, target int) int {
-	distances := m.walk(start, target)
-
-	solution := 0
-	for _, steps := range distances {
-		if steps%2 == target%2 {
-			solution++
-		}
-	}
-
-	return solution
-}
-
-func (m *Map) walk(start image.Point, target int) map[image.Point]int {
-	q := list.New()
-	q.PushBack(Record{Pos: start, Steps: 0})
-	dist := make(map[image.Point]int)
-	for q.Len() > 0 {
-		r := q.Remove(q.Front()).(Record)
-		if r.Steps == target {
-			break
-		}
-		for _, n := range m.next(r.Pos) {
-			if d, ok := dist[n]; !ok || r.Steps+1 < d {
-				dist[n] = r.Steps + 1
-				q.PushBack(Record{Pos: n, Steps: r.Steps + 1})
-			}
-		}
-	}
-
-	return dist
-}
-
-func abs(x int) int {
-	if x < 0 {
-		return -x
-	}
-	return x
-}
-
-type Record struct {
-	Pos   image.Point
-	Steps int
-}
-
-type Map struct {
-	rocks map[image.Point]bool
-	lim   image.Point
-}
-
-func (m *Map) next(pt image.Point) []image.Point {
-	var result []image.Point
-	for _, d := range []image.Point{{-1, 0}, {1, 0}, {0, -1}, {0, 1}} {
-		nxt := pt.Add(d)
-		if m.isFree(nxt) {
-			result = append(result, nxt)
-		}
-	}
-	return result
-}
-
-func (m *Map) nextNoWrap(pt image.Point) []image.Point {
-	var result []image.Point
-	for _, d := range []image.Point{{-1, 0}, {1, 0}, {0, -1}, {0, 1}} {
-		nxt := pt.Add(d)
-		if m.isPointInBounds(nxt) && m.isFree(nxt) {
-			result = append(result, nxt)
-		}
-	}
-	return result
-}
-
-func (m *Map) isPointInBounds(p image.Point) bool {
-	return p.X >= 0 && p.X < m.lim.X && p.Y >= 0 && p.Y < m.lim.Y
-}
-
-func (m *Map) isFree(p image.Point) bool {
-	if _, ok := m.rocks[mod(p, m.lim)]; ok {
-		return false
-	}
-	return true
-}
-
-func mod(pt, lim image.Point) image.Point {
-	return image.Point{
-		X: (pt.X%lim.X + lim.X) % lim.X,
-		Y: (pt.Y%lim.Y + lim.Y) % lim.Y,
-	}
-}
-
-func (m *Map) Dist(from image.Point, to image.Point) (int, error) {
-	if !m.isFree(from) {
-		return -1, fmt.Errorf("the starting point %v is not free", from)
-	}
-	if !m.isFree(to) {
-		return -1, fmt.Errorf("the target point %v is not free", to)
-	}
-	dist := map[image.Point]int{from: 0}
-	q := list.New()
-	q.PushBack(from)
-	for q.Len() > 0 {
-		p := q.Remove(q.Front()).(image.Point)
-		if p == to {
-			break
-		}
-		for _, n := range m.nextNoWrap(p) {
-			if _, ok := dist[n]; !ok {
-				dist[n] = dist[p] + 1
-				q.PushBack(n)
-			}
-		}
-	}
-	if d, ok := dist[to]; ok {
-		return d, nil
-	} else {
-		return -1, fmt.Errorf("no path found from %v to %v", from, to)
-	}
 }
