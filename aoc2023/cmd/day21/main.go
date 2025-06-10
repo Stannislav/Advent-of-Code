06ad81894ffacad3ef50cdadf2e5f6c82b8f423f
Stanislv Schmidt <Stannislav@users.noreply.github.com>
2025-06-10 11:18:34 +0200

    Solve part 2

--- aoc2023/cmd/day21/main.go
+++ aoc2023/cmd/day21/main.go
@@ -12,6 +12,190 @@ func main() {
 	start, m := ParseInput("input/21.txt")
 	fmt.Println("Part 1 :", Part1(start, &m))
 
+	fmt.Printf("Map size is %v\n", m.lim)
+
+	// Distances from start to corners - all 130!
+	from := start
+	tos := []image.Point{{0, 0}, {0, 130}, {130, 0}, {130, 130}}
+	cornerDistances := make(map[image.Point]int)
+	for _, to := range tos {
+		d, _ := m.Dist(from, to)
+		cornerDistances[to] = d
+	}
+	fmt.Printf("Distances from start to corners: %v\n", cornerDistances)
+
+	// Distances to cross through the centre - all 130!
+	dHorizontal, _ := m.Dist(image.Point{65, 0}, image.Point{65, 130})
+	fmt.Printf("Crossing through centre horizontally: %d\n", dHorizontal)
+	dVertical, _ := m.Dist(image.Point{0, 65}, image.Point{130, 65})
+	fmt.Printf("Crossing through centre vertically: %d\n", dVertical)
+
+	// Distances for crossing the map - not all 130 and fluctuating. So, no regularity.
+	crossingDistances := []int{}
+	for x := 0; x < m.lim.X; x++ {
+		d, _ := m.Dist(image.Point{x, 0}, image.Point{x, 130})
+		crossingDistances = append(crossingDistances, d)
+	}
+	fmt.Printf("Distances to cross: %v\n", crossingDistances)
+
+	// Maximal distances from edge centers - all 195 = 130 + 65.
+	// maxDistanceLeft := 0
+	// maxDistanceRight := 0
+	// maxDistanceTop := 0
+	// maxDistanceBottom := 0
+	// for x := 0; x < m.lim.X; x++ {
+	// 	for y := 0; y < m.lim.Y; y++ {
+	// 		if m.isFree(image.Point{x, y}) {
+	// 			dLeft, _ := m.Dist(image.Point{x, y}, image.Point{65, 0})
+	// 			dRight, _ := m.Dist(image.Point{x, y}, image.Point{65, 130})
+	// 			dTop, _ := m.Dist(image.Point{x, y}, image.Point{0, 65})
+	// 			dBottom, _ := m.Dist(image.Point{x, y}, image.Point{130, 65})
+	// 			if dLeft > maxDistanceLeft {
+	// 				maxDistanceLeft = dLeft
+	// 			}
+	// 			if dRight > maxDistanceRight {
+	// 				maxDistanceRight = dRight
+	// 			}
+	// 			if dTop > maxDistanceTop {
+	// 				maxDistanceTop = dTop
+	// 			}
+	// 			if dBottom > maxDistanceBottom {
+	// 				maxDistanceBottom = dBottom
+	// 			}
+	// 		}
+	// 	}
+	// }
+	// fmt.Printf("Max distances from edge centers: Left: %d, Right: %d, Top: %d, Bottom: %d\n", maxDistanceLeft, maxDistanceRight, maxDistanceTop, maxDistanceBottom)
+
+	// Compute reachable plots for the middle part and the corner parts.
+	fmt.Printf("Total number of plots: %d\n", m.lim.X*m.lim.Y-len(m.rocks))
+	fmt.Printf("Reachable plots in the middle part: %d\n", bruteForce(start, &m, 64)+bruteForce(start, &m, 65))
+	fmt.Println("Reachable plots from the top left corner:", bruteForce(image.Point{0, 0}, &m, 65))
+	fmt.Println("Reachable plots from the top right corner:", bruteForce(image.Point{0, 130}, &m, 65))
+	fmt.Println("Reachable plots from the bottom left corner:", bruteForce(image.Point{130, 0}, &m, 65))
+	fmt.Println("Reachable plots from the bottom right corner:", bruteForce(image.Point{130, 130}, &m, 65))
+
+	// Test scaling
+	solutions := []int{}
+	for scale := 0; scale <= 5; scale++ {
+		steps := 65 + scale*131
+		solution := bruteForce(start, &m, steps)
+		fmt.Printf("For scale %d, with %d steps, reachable plots: %d\n", scale, steps, solution)
+		solutions = append(solutions, solution)
+	}
+	fmt.Println("Differences")
+	for i := 1; i < len(solutions); i++ {
+		fmt.Printf("Scale %d: %d\n", i, solutions[i]-solutions[0])
+	}
+	n := 3
+	fmt.Printf("At %d: %d\n", n, bruteForce(start, &m, n))
+
+	c := bruteForce(start, &m, 65)
+	cPrime := bruteForce(start, &m, 64)
+	evenCoords := []image.Point{}
+	oddCoords := []image.Point{}
+	for x := 0; x < m.lim.X; x++ {
+		for y := 0; y < m.lim.Y; y++ {
+			pt := image.Point{X: x, Y: y}
+			if _, ok := m.rocks[pt]; !ok {
+				if (x+y)%2 == 0 {
+					evenCoords = append(evenCoords, pt)
+				} else {
+					oddCoords = append(oddCoords, pt)
+				}
+			}
+		}
+	}
+	x := len(evenCoords) - c
+	xPrime := len(oddCoords) - cPrime
+	fmt.Printf("c=%d\n", c)
+	fmt.Printf("c'=%d\n", cPrime)
+	fmt.Printf("x=%d\n", x)
+	fmt.Printf("x'=%d\n", xPrime)
+	fmt.Printf("c + c' + x + x' + rocks = %d\n", c+cPrime+x+xPrime+len(m.rocks))
+	fmt.Printf("131 * 131 = %d\n", 131*131)
+
+	distancesAll := m.walk(start, 65+131)
+	cDist := make(map[image.Point]int)
+	cPrimeDist := make(map[image.Point]int)
+	xDist := make(map[image.Point]int)
+	xPrimeDist := make(map[image.Point]int)
+
+	walkedCPrime := filterEven(m.walk(start, 64))
+	fmt.Printf("Walked c' from start: %d\n", len(walkedCPrime))
+	for x := 0; x < m.lim.X; x++ {
+		for y := 0; y < m.lim.Y; y++ {
+			pt := image.Point{x, y}
+			if _, reachable := distancesAll[pt]; !reachable {
+				continue
+			}
+			d := abs(pt.X-start.X) + abs(pt.Y-start.Y)
+			if d <= 65 {
+				if (x+y)%2 == 0 {
+					cPrimeDist[pt] = 0
+				} else {
+					cDist[pt] = 0
+				}
+			} else {
+				if (x+y)%2 == 0 {
+					xPrimeDist[pt] = 0
+				} else {
+					xDist[pt] = 0
+				}
+			}
+		}
+	}
+	fmt.Printf("c=%d, len(cDist)=%d\n", c, len(cDist))
+	fmt.Printf("c'=%d, len(cPrimeDist)=%d\n", cPrime, len(cPrimeDist))
+	fmt.Printf("x=%d, len(xDist)=%d\n", x, len(xDist))
+	fmt.Printf("x'=%d, len(xPrimeDist)=%d\n", xPrime, len(xPrimeDist))
+	fmt.Printf("Distances from start with target 65+131: %v\n", len(distancesAll))
+	for pt := range xDist {
+		d := abs(pt.X-start.X) + abs(pt.Y-start.Y)
+		if d <= 65 {
+			fmt.Printf("Point in xDist with dist <= 65: %v, dist: %d\n", pt, d)
+		}
+	}
+	distancesEven := filterEven(distancesAll)
+	distancesOdd := filterOdd(distancesAll)
+	fmt.Printf("Even distances: %d\nOdd distances: %d\n", len(distancesEven), len(distancesOdd))
+	fmt.Printf("According to formula expected even distances to be c + 4c' + 2(x + x') = %d\n", cPrime+4*c+2*(x+xPrime))
+	fmt.Printf("According to formula expected even distances to be c + 4c' + 2(x + x') = %d\n", len(cPrimeDist)+4*len(cDist)+2*(len(xDist)+len(xPrimeDist)))
+
+	cc := len(cDist)
+	ccPrime := len(cPrimeDist)
+	xx := len(xDist)
+	xxPrime := len(xPrimeDist)
+	cube := cc + xx
+	cubePrime := ccPrime + xxPrime
+	w := cube + cubePrime
+	fmt.Printf("According to formula expected even distances '1' to be c' + 4c + 2(x + x') = %d\n", ccPrime+4*cc+2*(xx+xxPrime))
+	fmt.Printf("According to formula expected even distances '2' to be 9c + 4c' + 6(x + x') = %d\n", 9*cc+4*ccPrime+6*(xx+xxPrime))
+	fmt.Printf("Cube-based formula for '1': cube + 4*cubePrime - 2*xxPrime + xx = %d\n", 4*cube+cubePrime+xxPrime-2*xx)
+
+	nn := 202300
+	result := nn*nn*w + (2*nn+1)*cube - (nn+1)*xx + nn*xxPrime
+	fmt.Printf("Cube-base formula for '%d' generalised: %d\n", nn, result)
+
+	// borderPoints := []image.Point{}
+	// for pt, _ := range dist {
+	// 	if abs(pt.X-start.X)+abs(pt.Y-start.Y) == target {
+	// 		borderPoints = append(borderPoints, pt)
+	// 	}
+	// }
+	// slices.SortFunc(borderPoints, func(a, b image.Point) int { return a.X - b.X })
+	// fmt.Printf("Border points: %v (%d)\n", borderPoints, len(borderPoints))
+	// for x := 0; x < m.lim.X; x++ {
+	// 	for y := 0; y < m.lim.Y; y++ {
+	// 		if _, ok := dist[image.Point{X: x, Y: y}]; ok {
+	// 			fmt.Print("#")
+	// 		} else {
+	// 			fmt.Print(".")
+	// 		}
+	// 	}
+	// 	fmt.Println()
+	// }
+
 }
 
 func ParseInput(filename string) (image.Point, Map) {
@@ -39,35 +223,79 @@ func ParseInput(filename string) (image.Point, Map) {
 	return start, m
 }
 
+func filterEven(distances map[image.Point]int) map[image.Point]int {
+	result := make(map[image.Point]int)
+	for pt, steps := range distances {
+		if steps%2 == 0 {
+			result[pt] = steps
+		}
+	}
+	return result
+}
+
+func filterOdd(distances map[image.Point]int) map[image.Point]int {
+	result := make(map[image.Point]int)
+	for pt, steps := range distances {
+		if steps%2 == 1 {
+			result[pt] = steps
+		}
+	}
+	return result
+}
+
+func setDiff(a, b map[image.Point]int) map[image.Point]int {
+	result := make(map[image.Point]int)
+	for pt, steps := range a {
+		if _, ok := b[pt]; !ok {
+			result[pt] = steps
+		}
+	}
+	return result
+}
+
 func Part1(start image.Point, m *Map) int {
 	return bruteForce(start, m, 64)
 }
 
 func bruteForce(start image.Point, m *Map, target int) int {
-	q := list.New()
-	q.PushBack(Record{Pos: start, Steps: 0})
-	dist := make(map[image.Point]int)
-	for q.Len() > 0 {
-		r := q.Remove(q.Front()).(Record)
-		cont := r.Steps < target
-		for _, n := range m.next(r.Pos) {
-			if d, ok := dist[n]; !ok || r.Steps+1 < d {
-				dist[n] = r.Steps + 1
-				if cont {
-					q.PushBack(Record{Pos: n, Steps: r.Steps + 1})
-				}
-			}
-		}
-	}
+	distances := m.walk(start, target)
+	fmt.Printf("Points reached with target %d: %d\n", target, len(distances))
 
 	solution := 0
-	for _, steps := range dist {
+	for _, steps := range distances {
 		if steps%2 == target%2 {
 			solution++
 		}
 	}
 
 	return solution
+}
+
+func (m *Map) walk(start image.Point, target int) map[image.Point]int {
+	q := list.New()
+	q.PushBack(Record{Pos: start, Steps: 0})
+	dist := make(map[image.Point]int)
+	for q.Len() > 0 {
+		r := q.Remove(q.Front()).(Record)
+		if r.Steps == target {
+			break
+		}
+		for _, n := range m.next(r.Pos) {
+			if d, ok := dist[n]; !ok || r.Steps+1 < d {
+				dist[n] = r.Steps + 1
+				q.PushBack(Record{Pos: n, Steps: r.Steps + 1})
+			}
+		}
+	}
+
+	return dist
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
 }
 
 type Record struct {
