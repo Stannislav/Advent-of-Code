ea83ed4cefd45e3d1e73c852c2776a44fc23b037
Stanislv Schmidt <Stannislav@users.noreply.github.com>
2025-06-10 13:43:27 +0200

    Add comments on part 2 solution

--- aoc2023/cmd/day21/main.go
+++ aoc2023/cmd/day21/main.go
@@ -15,6 +15,66 @@ func main() {
 }
 
 func Part2(start image.Point, m *Map) int {
+	/*
+		The solution to part 2 is not a generalisation of part 1. Instead, it relies on two specific
+		propreties of the input data and the problem statement:
+
+		1. The amount of steps required, 26501365, aligns with the size of the map: the map size is 131x131,
+		   and 26501365 = 65 + 202300 * 131. Starting from the centre of the map, (65, 65), one needs 65
+		   steps to reach the edge of the map. So, the maximal distance one will reach is exactly 202300 map
+		   tiles away from the central original map tile.
+		2. The rocks are placed in such a way that taking 65 + n * 131 steps from the centre for any n will cover
+		   all reachable points within that Manhattan distance.
+
+		Given these two properties, the solution can be found by counting the number of covered map tiles.
+		Because distances are measured in Manhattan metric, the reachable points form a rhombus shape around the
+		centre of the map. For example, for 65 steps, the original map tile is divided into 5 parts: the central
+		diamond, and the four corner triangles. These parts is all that is needed to compose the final big rhombus
+		of reachable points.
+
+		An additional detail which needs to be taken into account is whether the step number is even or odd. For an
+		even step number only the reachable points with even Manhattan distance from the centre are counted, and
+		analogously for the odd step number. So, each of the five parts of the map tile is divided into two
+		subparts: one with even and one with odd distances.
+
+		Here's a schematic of the five parts:
+
+		 ______
+		|  /\  |
+		|a/  \b|
+		|/ M  \|
+		|\    /| = W
+		|c\  /d|
+		|__\/__|
+
+		Let's make the following definitions:
+
+		- M: all reachable points in the central rhombus.
+		- a, b, c, d: all reachable points in the four corner parts.
+		- x := a + b + c + d: all reachable corner points.
+		- W := M + x: all reachable points in the original map tile.
+		- <symbol>_e, <symbol>_o: even and odd points in <symbol>, meaning the distances from the centre point is even or odd.
+		- N_n: the number of points which can be reached after exactly 65 + n * 131 steps.
+
+		By drawing the examples of the step coverage for the first few values of n one can convince oneself that
+		N_n are given by the following:
+
+		N_0 = M_o
+		N_1 = M_e + 4M_o + 2x
+		N_2 = M_o + 4M_e + 8M_o + 6x
+		N_3 = M_e + 4M_o + 8M_e +  12M_o + 12x
+
+		These can be rewritten in terms of whole whole tiles W:
+
+		N_0 =       W_o -  x_o
+		N_1 =  W + 3W_o - 2x_o +  x_e
+		N_2 = 4W + 5W_o - 3x_o + 2x_e
+		N_3 = 9W + 7W_o - 4x_o + 3x_e
+
+		We can now read off the general formula:
+
+		N_n = n^2W + 2(n+1)W_o - (n+1)x_o + nx_e
+	*/
 	fmt.Printf("Map size is %v\n", m.lim)
 
 	// Distances from start to corners - all 130!
@@ -195,6 +255,7 @@ func Part2(start image.Point, m *Map) int {
 	// 	fmt.Println()
 	// }
 	nn := 202300
+	// N_n = n^2W + 2(n+1)W_o - (n+1)x_o + nx_e
 	return nn*nn*w + (2*nn+1)*cube - (nn+1)*xx + nn*xxPrime
 }
 
