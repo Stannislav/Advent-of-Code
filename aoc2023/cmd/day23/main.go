c3c3355df8816a4ce33075f91a7471d64a634be1
Stanislv Schmidt <Stannislav@users.noreply.github.com>
2025-06-10 19:53:46 +0200

    Solve part 1

--- aoc2023/cmd/day23/main.go
+++ aoc2023/cmd/day23/main.go
@@ -7,20 +7,84 @@ import (
 	"strings"
 )
 
+type Map struct {
+	Grid  map[image.Point]rune
+	Start image.Point
+	End   image.Point
+	Dim   image.Rectangle
+}
+
+var slopeDirections = map[rune]image.Point{'^': image.Pt(-1, 0), '>': image.Pt(0, 1), 'v': image.Pt(1, 0), '<': image.Pt(0, -1)}
+var deltas = []image.Point{image.Pt(-1, 0), image.Pt(0, 1), image.Pt(1, 0), image.Pt(0, -1)}
+
 func main() {
-	fmt.Printf("Part 1: %d\n", 0)
+	m := ParseInput("input/23.txt")
+
+	fmt.Printf("Part 1: %d\n", Part1(m))
 	fmt.Printf("Part 2: %d\n", 0)
 }
 
-func ParseInput(filename string) map[image.Point]rune {
-	inputBytes, _ := os.ReadFile(filename)
+func ParseInput(filename string) *Map {
+	inputBytes, err := os.ReadFile(filename)
+	if err != nil {
+		panic(fmt.Sprintf("file %s not found", filename))
+	}
 
-	m := make(map[image.Point]rune)
+	grid := make(map[image.Point]rune)
 	for row, line := range strings.Fields(string(inputBytes)) {
 		for col, r := range line {
-			m[image.Point{row, col}] = r
+			grid[image.Point{row, col}] = r
 		}
 	}
 
-	return m
+	dim := image.Rectangle{}
+	for pt := range grid {
+		dim = dim.Union(image.Rectangle{image.Pt(0, 0), pt})
+	}
+
+	return &Map{grid, dim.Min.Add(image.Pt(0, 1)), dim.Max.Sub(image.Pt(0, 1)), dim}
+}
+
+func Part1(m *Map) int {
+	return m.LongestPath(map[image.Point]int{m.Start: 0}, m.Start)
+}
+
+func (m *Map) LongestPath(currentPath map[image.Point]int, start image.Point) int {
+	if start.Eq(m.End) {
+		return currentPath[start]
+	}
+
+	distances := []int{}
+	for _, next := range m.next(start) {
+		if _, seen := currentPath[next]; seen {
+			continue
+		}
+		currentPath[next] = currentPath[start] + 1
+		distances = append(distances, m.LongestPath(currentPath, next))
+		delete(currentPath, next)
+	}
+
+	result := 0
+	for _, d := range distances {
+		if d > result {
+			result = d
+		}
+	}
+
+	return result
+}
+
+func (m *Map) next(pt image.Point) []image.Point {
+	if delta, ok := slopeDirections[m.Grid[pt]]; ok {
+		return []image.Point{pt.Add(delta)}
+	}
+
+	result := []image.Point{}
+	for _, delta := range deltas {
+		next := pt.Add(delta)
+		if r, ok := m.Grid[next]; ok && r != '#' {
+			result = append(result, next)
+		}
+	}
+	return result
 }
--- aoc2023/cmd/day23/main_test.go
+++ aoc2023/cmd/day23/main_test.go
@@ -7,8 +7,8 @@ import (
 
 func TestParseInput(t *testing.T) {
 	m := ParseInput("example_input.txt")
-	if len(m) != 23*23 {
-		t.Errorf("Expected the number of map points to be 23*23=%d, but got %d\n", 23*23, len(m))
+	if len(m.Grid) != 23*23 {
+		t.Errorf("Expected the number of map points to be 23*23=%d, but got %d\n", 23*23, len(m.Grid))
 	}
 
 	test_cases := []struct {
@@ -24,8 +24,24 @@ func TestParseInput(t *testing.T) {
 	}
 
 	for _, tc := range test_cases {
-		if m[tc.pt] != tc.r {
-			t.Errorf("Expected the map at point %v to be %c but got %c", tc.pt, tc.r, m[tc.pt])
+		if m.Grid[tc.pt] != tc.r {
+			t.Errorf("Expected the map at point %v to be %c but got %c", tc.pt, tc.r, m.Grid[tc.pt])
 		}
+	}
+
+	if !m.Start.Eq(image.Pt(0, 1)) {
+		t.Errorf("Expected start to be (0, 1), but got %v", m.Start)
+	}
+	if !m.End.Eq(image.Pt(22, 21)) {
+		t.Errorf("Expected end to be (22, 21), but got %v", m.End)
+	}
+}
+
+func TestPart1(t *testing.T) {
+	m := ParseInput("example_input.txt")
+	result := Part1(m)
+	expected := 94
+	if result != expected {
+		t.Errorf("Expected result to be %d, but got %d", expected, result)
 	}
 }
